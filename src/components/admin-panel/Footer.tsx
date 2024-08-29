f883d38d3a0deaa18389dc5911d4daf70a37f749
eleo romario <eleo.romario@gmail.com>
2024-08-29 11:57:32 -0500

    chore: actualizar diseño del pie de página en el panel de administración

--- src/components/admin-panel/Footer.tsx
+++ src/components/admin-panel/Footer.tsx
@@ -1,14 +1,11 @@
-import { HelpCircle } from "lucide-react";
-
 const infoBussiness = {
   url: "https://chaqchao.com",
-  email: "chaqchao@gmail.com",
 };
 export default function Footer() {
   const currentYear = new Date().getFullYear();
   return (
-    <div className="z-20 w-full bg-background/95 shadow backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="mx-4 flex h-auto flex-col items-center sm:h-14 sm:justify-between md:mx-8 md:flex-row">
+    <div className="z-20 w-full border-t-[1px] border-slate-100 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+      <div className="mx-4 flex h-auto flex-col items-center sm:h-14 sm:justify-end md:mx-8 md:flex-row">
         <div className="mb-4 text-sm text-muted-foreground md:mb-0">
           © {currentYear}{" "}
           <a
@@ -18,16 +15,6 @@ export default function Footer() {
             className="text-sm text-muted-foreground hover:text-primary"
           >
             Chaqchao
-          </a>
-          . All rights reserved.
-        </div>
-        <div className="flex flex-wrap justify-center gap-4 md:justify-end">
-          <a
-            href={`email:${infoBussiness.email}`}
-            className="flex items-center text-sm text-muted-foreground hover:text-primary"
-          >
-            <HelpCircle className="mr-1 h-4 w-4" />
-            Help & Support
           </a>
         </div>
       </div>
