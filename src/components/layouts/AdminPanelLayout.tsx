c23151805139d077854e70cc8cda87c07ef1a16a
eleo romario <eleo.romario@gmail.com>
2024-08-29 10:47:47 -0500

    feat: agregar componente AdminPanelLayout al panel de administración

---
+++ src/components/layouts/AdminPanelLayout.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { useSidebarToggle } from "@/hooks/use-sidebar-toggle";
+import { useStore } from "@/hooks/use-store";
+
+import { cn } from "@/lib/utils";
+
+import Footer from "../admin-panel/Footer";
+import Sidebar from "../admin-panel/Sidebar";
+
+export default function AdminPanelLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const sidebar = useStore(useSidebarToggle, (state) => state);
+
+  if (!sidebar) return null;
+
+  return (
+    <>
+      <Sidebar />
+      <main
+        className={cn(
+          "min-h-[calc(100vh_-_56px)] bg-zinc-50 transition-[margin-left] duration-300 ease-in-out dark:bg-zinc-900",
+          sidebar?.isOpen === false ? "lg:ml-[90px]" : "lg:ml-72",
+        )}
+      >
+        {children}
+      </main>
+      <footer
+        className={cn(
+          "transition-[margin-left] duration-300 ease-in-out",
+          sidebar?.isOpen === false ? "lg:ml-[90px]" : "lg:ml-72",
+        )}
+      >
+        <Footer />
+      </footer>
+    </>
+  );
+}
