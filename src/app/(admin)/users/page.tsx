bb040a327346447c5b7b1c4264cd4de6001b389e
eleo romario <eleo.romario@gmail.com>
2024-08-29 11:57:15 -0500

    feat: actualizar página de usuarios en el panel de administración

--- src/app/(admin)/users/page.tsx
+++ src/app/(admin)/users/page.tsx
@@ -1,16 +1,3 @@
-import { Navbar } from "@/components/admin-panel/Navbar";
-
-export default function PageUsers({
-  children,
-  title,
-}: {
-  children: React.ReactNode;
-  title: string;
-}) {
-  return (
-    <>
-      <Navbar title={title} />
-      <div className="container px-4 pb-8 pt-8 sm:px-8">{children}</div>
-    </>
-  );
+export default function PageUsers() {
+  return <>padge user</>;
 }
