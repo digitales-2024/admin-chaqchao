233815e8988b9efd3d2612b973b2291f4a24ccbc
eleo romario <eleo.romario@gmail.com>
2024-08-29 11:59:45 -0500

    feat: agregar componente Navbar al diseño del panel de administración

--- src/components/layouts/AdminPanelLayout.tsx
+++ src/components/layouts/AdminPanelLayout.tsx
@@ -6,6 +6,7 @@ import { useStore } from "@/hooks/use-store";
 import { cn } from "@/lib/utils";
 
 import Footer from "../admin-panel/Footer";
+import { Navbar } from "../admin-panel/Navbar";
 import Sidebar from "../admin-panel/Sidebar";
 
 export default function AdminPanelLayout({
@@ -22,11 +23,12 @@ export default function AdminPanelLayout({
       <Sidebar />
       <main
         className={cn(
-          "min-h-[calc(100vh_-_56px)] bg-zinc-50 transition-[margin-left] duration-300 ease-in-out dark:bg-zinc-900",
+          "min-h-[calc(100vh_-_57px)] bg-zinc-50 transition-[margin-left] duration-300 ease-in-out dark:bg-zinc-900",
           sidebar?.isOpen === false ? "lg:ml-[90px]" : "lg:ml-72",
         )}
       >
-        {children}
+        <Navbar />
+        <div className="container px-4 pb-8 pt-8 sm:px-8">{children}</div>
       </main>
       <footer
         className={cn(
