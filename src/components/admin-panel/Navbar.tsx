b0e5dd7cc438349dc660610682e0b9c9de40456b
eleo romario <eleo.romario@gmail.com>
2024-08-29 11:58:04 -0500

    feat: actualizar diseño del pie de página en el panel de administración

--- src/components/admin-panel/Navbar.tsx
+++ src/components/admin-panel/Navbar.tsx
@@ -2,17 +2,12 @@ import { Notifications } from "./Notifications";
 import { SheetMenu } from "./SheetMenu";
 import { UserNav } from "./UserNav";
 
-interface NavbarProps {
-  title: string;
-}
-
-export const Navbar = ({ title }: NavbarProps) => {
+export const Navbar = () => {
   return (
-    <header className="sticky top-0 z-10 w-full bg-background/95 shadow-sm backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <header className="sticky top-0 z-10 w-full border-b-[1px] border-slate-100 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="mx-4 flex h-14 items-center sm:mx-8">
         <div className="flex items-center space-x-4 lg:space-x-0">
           <SheetMenu />
-          <h1 className="font-bold">{title}</h1>
         </div>
         <div className="flex flex-1 items-center justify-end gap-2 space-x-2">
           <Notifications />
