0902e8fdad7d811e01b2f29ff3ab7ec04d3d5a16
eleo romario <eleo.romario@gmail.com>
2024-09-02 16:20:10 -0500

    feat: agregar tipo de usuario para manejar datos de usuario autenticado

--- src/types/login.ts
+++ src/types/login.ts
@@ -2,3 +2,12 @@ export type Credentials = {
   email: string;
   password: string;
 };
+
+export type User = {
+  id: number;
+  name: string;
+  email: string;
+  phone: string;
+  roles: { id: string; name: string }[];
+  token: string;
+};
