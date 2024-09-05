e579cd6f68ee2936c79d3ee46195df30e97c71e0
eleo romario <eleo.romario@gmail.com>
2024-09-05 17:30:36 -0500

    feat: agregar la fuente fallback y el preload

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,14 @@
 import { Providers } from "@/redux/providers";
 import type { Metadata } from "next";
 import { Lexend_Deca } from "next/font/google";
+import { Toaster } from "sonner";
 import "./globals.css";
 
-const lexend = Lexend_Deca({ subsets: ["latin"] });
+const lexend = Lexend_Deca({
+  subsets: ["latin"],
+  fallback: ["system-ui", "arial"],
+  preload: true,
+});
 
 export const metadata: Metadata = {
   title: "Chaqchao - Panel Administrativo",
@@ -18,6 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={lexend.className}>
+        <Toaster richColors />
         <Providers>{children}</Providers>
       </body>
     </html>
