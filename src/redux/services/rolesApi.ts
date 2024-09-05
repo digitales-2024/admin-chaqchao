63af550c70d1807a2f9b71feadd7c57971787d81
eleo romario <eleo.romario@gmail.com>
2024-09-05 17:32:05 -0500

    feat: agregar servicio para gestionar roles

---
+++ src/redux/services/rolesApi.ts
@@ -0,0 +1,67 @@
+import { TOKEN } from "@/constants";
+import { Role } from "@/types/roles";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import Cookies from "js-cookie";
+
+export const rolesApi = createApi({
+  reducerPath: "rolesApi",
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_BACKEND_URL }),
+  tagTypes: ["Roles"],
+  endpoints: (build) => ({
+    // Obtener todos los roles
+    getRoles: build.query<Role[], void>({
+      query: () => ({
+        url: `/rol`,
+        headers: {
+          Authorization: `Bearer ${Cookies.get(TOKEN)}`,
+        },
+      }),
+      providesTags: ["Roles"],
+    }),
+
+    // Obtener un rol por id
+    getRole: build.query<Role, { id?: string }>({
+      query: (id) => ({
+        url: `rol/${id}`,
+        headers: {
+          Authorization: `Bearer ${Cookies.get(TOKEN)}`,
+        },
+      }),
+    }),
+
+    // Crear un nuevo rol
+    createRole: build.mutation<Role, Partial<Role>>({
+      query: (body) => ({
+        url: `roles`,
+        method: "POST",
+        body,
+      }),
+      invalidatesTags: ["Roles"],
+    }),
+    // Actualizar un rol por id
+    updateRole: build.mutation<Role, Partial<Role>>({
+      query: ({ id, ...body }) => ({
+        url: `roles/${id}`,
+        method: "PATCH",
+        body,
+      }),
+      invalidatesTags: ["Roles"],
+    }),
+    // Eliminar un rol por id
+    deleteRole: build.mutation<void, number>({
+      query: (id) => ({
+        url: `roles/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["Roles"],
+    }),
+  }),
+});
+
+export const {
+  useGetRolesQuery,
+  useGetRoleQuery,
+  useCreateRoleMutation,
+  useUpdateRoleMutation,
+  useDeleteRoleMutation,
+} = rolesApi;
