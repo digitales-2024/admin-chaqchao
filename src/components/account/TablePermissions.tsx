c6c6bcdaa0406aa861c5d484852966400125c502
eleo romario <eleo.romario@gmail.com>
2024-09-10 14:29:49 -0500

    feat: mostrar la tabla de Permisos en el componente TablePermissions

--- src/components/account/TablePermissions.tsx
+++ src/components/account/TablePermissions.tsx
@@ -1,48 +1,91 @@
+import { useAuth } from "@/hooks/use-auth";
+import { useGetRoleQuery } from "@/redux/services/rolesApi";
+import { Module, Permission } from "@/types";
+import { extractUniquePermissions } from "@/utils/extractUniquePermissions";
+import { ChevronDown, ChevronUp } from "lucide-react";
+import { useEffect, useState } from "react";
+
+import { Badge } from "../ui/badge";
+import { Button } from "../ui/button";
 import { ScrollArea } from "../ui/scroll-area";
 import {
   Table,
   TableBody,
   TableCaption,
+  TableCell,
   TableHead,
   TableHeader,
   TableRow,
 } from "../ui/table";
 
 export const TablePermissions = () => {
-  // const [expandedModule, setExpandedModule] = useState<string | null>(null);
+  const [expandedModule, setExpandedModule] = useState<string | null>(null);
 
-  // const toggleModule = (moduleName: string) => {
-  //   setExpandedModule(expandedModule === moduleName ? null : moduleName);
-  // };
+  const toggleModule = (moduleName: string) => {
+    setExpandedModule(expandedModule === moduleName ? null : moduleName);
+  };
 
-  // const { user } = useAuth();
-
-  // const { data } = useGetRoleQuery(user?.id ?? 0);
+  const { user } = useAuth();
+  const { data } = useGetRoleQuery(user?.roles[0].id as string);
+  const [headerPermissions, setHeaderPermissions] = useState<Permission[]>();
+  useEffect(() => {
+    if (data) {
+      const uniquePermissions = extractUniquePermissions(data);
+      setHeaderPermissions(uniquePermissions);
+    }
+  }, [data]);
 
   return (
     <div className="w-full">
       {/* Desktop view */}
       <div className="hidden md:block">
-        <ScrollArea className="h-[400px] rounded-md border">
+        <ScrollArea className="h-fit rounded-md border">
           <Table>
             <TableCaption>Todos los permisos de este rol</TableCaption>
             <TableHeader>
               <TableRow>
-                <TableHead className="w-[100px]">Módulo</TableHead>
-                {/* {allPermissions.map((permission) => (
-                  <TableHead key={permission.cod}>{permission.label}</TableHead>
-                ))} */}
+                <TableHead className="uppercase">Módulo</TableHead>
+                {headerPermissions?.map((permission) => (
+                  <TableHead key={permission.id} className="uppercase">
+                    {permission.name}
+                  </TableHead>
+                ))}
               </TableRow>
             </TableHeader>
             <TableBody>
-              {/* {data?.modulePermissions.map((module) => (
-                <TableRow key={module.id}>
-                  <TableCell className="font-medium">{module.name}</TableCell>
-                  {allPermissions.map((permission) => (
-                    <TableCell key={permission.cod}></TableCell>
-                  ))} 
-                </TableRow>
-              ))}*/}
+              {data?.rolPermissions.map(
+                ({
+                  module,
+                  permissions,
+                }: {
+                  module: Module;
+                  permissions: Permission[];
+                }) => (
+                  <TableRow key={module.id}>
+                    <TableCell className="font-medium uppercase">
+                      {module.name}
+                    </TableCell>
+                    {headerPermissions?.map((permission: Permission) => (
+                      <TableCell key={permission.cod}>
+                        {permissions?.some(
+                          (perm) => perm.cod === permission.cod,
+                        ) ? (
+                          <Badge
+                            variant="default"
+                            className="bg-green-500 hover:bg-green-600"
+                          >
+                            ✓
+                          </Badge>
+                        ) : (
+                          <Badge variant="outline" className="text-gray-400">
+                            -
+                          </Badge>
+                        )}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                ),
+              )}
             </TableBody>
           </Table>
         </ScrollArea>
@@ -50,37 +93,45 @@ export const TablePermissions = () => {
 
       {/* Mobile view */}
       <div className="md:hidden">
-        {/* {data?.modulePermissions.map((module) => (
-          <div key={module.name} className="mb-4 rounded-md border">
-            <Button
-              variant="ghost"
-              className="w-full justify-between p-4 text-left"
-              onClick={() => toggleModule(module.name)}
-            >
-              {module.name}
-              {expandedModule === module.name ? (
-                <ChevronUp className="h-4 w-4" />
-              ) : (
-                <ChevronDown className="h-4 w-4" />
+        {data?.rolPermissions.map(
+          ({
+            module,
+            permissions,
+          }: {
+            module: Module;
+            permissions: Permission[];
+          }) => (
+            <div key={module.name} className="mb-4 rounded-md border">
+              <Button
+                variant="ghost"
+                className="w-full justify-between p-4 text-left"
+                onClick={() => toggleModule(module.name)}
+              >
+                <span className="uppercase">{module.name}</span>
+                {expandedModule === module.name ? (
+                  <ChevronUp className="h-4 w-4" />
+                ) : (
+                  <ChevronDown className="h-4 w-4" />
+                )}
+              </Button>
+              {expandedModule === module.name && (
+                <div className="p-4">
+                  {permissions.map((permission) => (
+                    <div
+                      key={permission.cod}
+                      className="mb-2 flex items-center justify-between"
+                    >
+                      <span className="capitalize">{permission.name}</span>
+                      <Badge variant="default" className="bg-green-500">
+                        ✓
+                      </Badge>
+                    </div>
+                  ))}
+                </div>
               )}
-            </Button>
-            {expandedModule === module.name && (
-              <div className="p-4">
-                {Object.entries(module.permissions).map(([key, permission]) => (
-                  <div
-                    key={key}
-                    className="mb-2 flex items-center justify-between"
-                  >
-                    <span>{permission.description}</span>
-                    <Badge variant="default" className="bg-green-500">
-                      ✓
-                    </Badge>
-                  </div>
-                ))}
-              </div>
-            )}
-          </div>
-        ))} */}
+            </div>
+          ),
+        )}
       </div>
     </div>
   );
