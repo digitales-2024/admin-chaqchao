30b1f94421270a19530460ec53bde786744660e1
eleo romario <eleo.romario@gmail.com>
2024-09-10 11:08:39 -0500

    feat: actualizar y mejorar el hook para manejar la informacion básica del perfil

--- src/hooks/use-profile.ts
+++ src/hooks/use-profile.ts
@@ -1,9 +1,50 @@
+import { useProfileQuery } from "@/redux/services/adminApi";
+import { useUpdateUserMutation } from "@/redux/services/usersApi";
+import { CustomErrorData, User } from "@/types";
+import { translateError } from "@/utils/translateError";
+import { toast } from "sonner";
+
+import { FormUpdateInfoProps } from "@/components/account/FormUpdateInfo";
+
 import { useAuth } from "./use-auth";
 
 export const useProfile = () => {
-  const { user } = useAuth();
+  const { setUser } = useAuth();
+
+  const { data: user, refetch } = useProfileQuery();
+  const [updateUser, { isLoading }] = useUpdateUserMutation();
+
+  const onUpdate = async (dataForm: FormUpdateInfoProps) => {
+    const promise = () =>
+      new Promise(async (resolve, reject) => {
+        try {
+          const result = await updateUser({ id: user?.id, ...dataForm });
+          if (result.error && "data" in result.error) {
+            const error = (result.error.data as CustomErrorData).message;
+            const message = translateError(error as string);
+            reject(new Error(message));
+          }
+          resolve(result);
+
+          setUser(result?.data?.data as User);
+          refetch();
+        } catch (error) {
+          reject(error);
+        }
+      });
+
+    toast.promise(promise(), {
+      loading: "Actualizando información...",
+      success: "información actualizado correctamente",
+      error: (error) => {
+        return error.message;
+      },
+    });
+  };
 
   return {
     user,
+    onUpdate,
+    isLoading,
   };
 };
