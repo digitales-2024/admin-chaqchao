1dc3c6a9095051bd2a3a587e1d88a9100f22d8b2
eleo romario <eleo.romario@gmail.com>
2024-09-11 17:23:25 -0500

    feat: actualizar diseño de tarjeta en el layout de administrador

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,9 +1,14 @@
 import AdminPanelLayout from "@/components/layouts/AdminPanelLayout";
+import { Card } from "@/components/ui/card";
 
 export default function AdminLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <AdminPanelLayout>{children}</AdminPanelLayout>;
+  return (
+    <AdminPanelLayout>
+      <Card className="border-none">{children}</Card>
+    </AdminPanelLayout>
+  );
 }
