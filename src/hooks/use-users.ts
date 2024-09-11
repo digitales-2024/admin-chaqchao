aa6f03d372dc6f891609e924ea6707438ade01ec
eleo romario <eleo.romario@gmail.com>
2024-09-11 16:56:25 -0500

    feat: agregar hook useUsers para obtener y manejar datos de usuarios

---
+++ src/hooks/use-users.ts
@@ -0,0 +1,6 @@
+import { useGetUsersQuery } from "@/redux/services/usersApi";
+
+export const useUsers = () => {
+  const { data, error, isLoading } = useGetUsersQuery();
+  return { data, error, isLoading };
+};
