b6d5f60fca55fa890401a176fbdd6e8928508c01
eleo romario <eleo.romario@gmail.com>
2024-09-12 15:18:29 -0500

    feat: actualizar DataTableViewOptions para mostrar las columnas en todos los tamaños de pantalla

--- src/components/users/UsersTable.tsx
+++ src/components/users/UsersTable.tsx
@@ -5,17 +5,18 @@ import { User } from "@/types";
 import { useMemo } from "react";
 
 import { DataTable } from "../data-table/DataTable";
-import { getColumns } from "./UsersTableColumns";
+import { usersColumns } from "./UsersTableColumns";
 import { UsersTableToolbarActions } from "./UsersTableToolbarActions";
 
 export function UsersTable({ data }: { data: User[] }) {
-  const columns = useMemo(() => getColumns(), []);
+  const columns = useMemo(() => usersColumns(), []);
 
   return (
     <DataTable
       data={data}
       columns={columns}
       toolbarActions={<UsersTableToolbarActions />}
+      placeholder="Buscar usuarios..."
     />
   );
 }
