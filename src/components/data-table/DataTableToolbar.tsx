0eb25d42f4f4bcbc67186086bbcadb4da12de996
eleo romario <eleo.romario@gmail.com>
2024-09-12 16:39:59 -0500

    feat: actualizar estilos de DataTableToolbar

--- src/components/data-table/DataTableToolbar.tsx
+++ src/components/data-table/DataTableToolbar.tsx
@@ -47,7 +47,7 @@ export function DataTableToolbar<TData>({
           </Button>
         )}
       </div>
-      <div className="flex items-center justify-end gap-2">
+      <div className="flex flex-wrap items-center justify-end gap-2">
         {toolbarActions ? cloneElement(toolbarActions, { table }) : null}
         {viewOptions && <DataTableViewOptions table={table} />}
       </div>
