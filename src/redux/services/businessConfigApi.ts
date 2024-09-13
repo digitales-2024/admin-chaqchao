59795b3b19091cda57d5a8e28b67375455fa6425
Diego Rondón <furrtr4532@gmail.com>
2024-09-13 14:56:35 -0500

    feat: creación de las funciones correspondientes a business config traidos del api

---
+++ src/redux/services/businessConfigApi.ts
@@ -0,0 +1,77 @@
+import { BusinessConfigData } from "@/types"; // Asegúrate de que los tipos estén correctos
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const businessConfigApi = createApi({
+  reducerPath: "businessConfigApi",
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_BACKEND_URL }),
+  tagTypes: ["BusinessConfig"],
+  endpoints: (build) => ({
+    // Mutación para crear una configuración de negocio
+    createBusinessConfig: build.mutation<
+      BusinessConfigData, // Respuesta esperada
+      {
+        businessName: string;
+        contactNumber: string;
+        email: string;
+        address: string;
+      } // Parámetros de entrada
+    >({
+      query: (body) => ({
+        url: "/business-config",
+        method: "POST",
+        body,
+        credentials: "include",
+      }),
+      invalidatesTags: ["BusinessConfig"],
+    }),
+
+    // Mutación para actualizar una configuración de negocio
+    updateBusinessConfig: build.mutation<
+      BusinessConfigData, // Respuesta esperada
+      {
+        id: string;
+        businessName?: string;
+        contactNumber?: string;
+        email?: string;
+        address?: string;
+      } // Parámetros de entrada
+    >({
+      query: ({ id, ...body }) => ({
+        url: `/business-config/${id}`,
+        method: "PATCH",
+        body,
+        credentials: "include",
+      }),
+      invalidatesTags: ["BusinessConfig"],
+    }),
+
+    // Consulta para obtener una configuración de negocio por ID
+    getBusinessConfigById: build.query<
+      BusinessConfigData, // Respuesta esperada
+      string // ID como parámetro
+    >({
+      query: (id) => ({
+        url: `/business-config/${id}`,
+        method: "GET",
+        credentials: "include",
+      }),
+      providesTags: (result, error, id) => [{ type: "BusinessConfig", id }],
+    }),
+
+    getBusinessConfigAll: build.query<BusinessConfigData[], void>({
+      query: () => ({
+        url: "/business-config",
+        method: "GET",
+        credentials: "include",
+      }),
+      providesTags: ["BusinessConfig"],
+    }),
+  }),
+});
+
+export const {
+  useCreateBusinessConfigMutation,
+  useUpdateBusinessConfigMutation,
+  useGetBusinessConfigByIdQuery,
+  useGetBusinessConfigAllQuery,
+} = businessConfigApi;
