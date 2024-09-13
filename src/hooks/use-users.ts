89dd3b05bbfbd234ada5bdf503dfb13fcc11f35c
EleoRomario <eleo.romario@gmail.com>
2024-09-12 21:24:10 -0500

    feat: agregar generación de contraseña en el hook useUsers

--- src/hooks/use-users.ts
+++ src/hooks/use-users.ts
@@ -1,6 +1,46 @@
-import { useGetUsersQuery } from "@/redux/services/usersApi";
+import {
+  useGeneratePasswordMutation,
+  useGetUsersQuery,
+} from "@/redux/services/usersApi";
+import { CustomErrorData } from "@/types";
+import { translateError } from "@/utils/translateError";
+import { toast } from "sonner";
 
 export const useUsers = () => {
   const { data, error, isLoading } = useGetUsersQuery();
-  return { data, error, isLoading };
+  const [generatePassword, { data: password }] = useGeneratePasswordMutation();
+
+  const handleGeneratePassword = async () => {
+    const promise = () =>
+      new Promise(async (resolve, reject) => {
+        try {
+          const result = await generatePassword(null);
+          if (result.error && "data" in result.error) {
+            const error = (result.error.data as CustomErrorData).message;
+            const message = translateError(error as string);
+            reject(new Error(message));
+          }
+          if (result.error) {
+            reject(
+              new Error(
+                "Ocurrió un error inesperado, por favor intenta de nuevo",
+              ),
+            );
+          }
+          resolve(result);
+        } catch (error) {
+          reject(error);
+        }
+      });
+
+    toast.promise(promise(), {
+      loading: "Generando...",
+      success: "Contraseña generada",
+      error: (error) => {
+        return error.message;
+      },
+    });
+  };
+
+  return { data, error, isLoading, handleGeneratePassword, password };
 };
