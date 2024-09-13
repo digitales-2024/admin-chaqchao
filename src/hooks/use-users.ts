f65922848bbf9c55e6cfbf6c37ded81c01744a1a
eleo romario <eleo.romario@gmail.com>
2024-09-13 11:37:25 -0500

    feat: agregar generación de contraseña en el hook useUsers y función para crear usuarios

--- src/hooks/use-users.ts
+++ src/hooks/use-users.ts
@@ -1,15 +1,17 @@
 import {
+  useCreateUserMutation,
   useGeneratePasswordMutation,
   useGetUsersQuery,
 } from "@/redux/services/usersApi";
-import { CustomErrorData } from "@/types";
+import { CustomErrorData, User } from "@/types";
 import { translateError } from "@/utils/translateError";
 import { toast } from "sonner";
 
 export const useUsers = () => {
   const { data, error, isLoading } = useGetUsersQuery();
   const [generatePassword, { data: password }] = useGeneratePasswordMutation();
-
+  const [createUser, { isSuccess: isSuccessCreateUser }] =
+    useCreateUserMutation();
   const handleGeneratePassword = async () => {
     const promise = () =>
       new Promise(async (resolve, reject) => {
@@ -42,5 +44,45 @@ export const useUsers = () => {
     });
   };
 
-  return { data, error, isLoading, handleGeneratePassword, password };
+  const onCreateUser = async (input: Partial<User>) => {
+    const promise = () =>
+      new Promise(async (resolve, reject) => {
+        try {
+          const result = await createUser(input);
+          if (result.error && "data" in result.error) {
+            const error = (result.error.data as CustomErrorData).message;
+            const message = translateError(error as string);
+            reject(new Error(message));
+          }
+          if (result.error) {
+            reject(
+              new Error(
+                "Ocurrió un error inesperado, por favor intenta de nuevo",
+              ),
+            );
+          }
+          resolve(result);
+        } catch (error) {
+          reject(error);
+        }
+      });
+
+    toast.promise(promise(), {
+      loading: "Creando...",
+      success: "Usuario creado",
+      error: (error) => {
+        return error.message;
+      },
+    });
+  };
+
+  return {
+    data,
+    error,
+    isLoading,
+    handleGeneratePassword,
+    password,
+    onCreateUser,
+    isSuccessCreateUser,
+  };
 };
