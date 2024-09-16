5cd830ff32db181ff5436c5b8ec4f5f364321877
Diego Rondón <furrtr4532@gmail.com>
2024-09-16 17:02:46 -0500

    feat: llamado de las funciones para actualizar y crear

--- src/hooks/use-business-config.ts
+++ src/hooks/use-business-config.ts
@@ -1,4 +1,11 @@
-import { useGetBusinessConfigAllQuery } from "@/redux/services/businessConfigApi";
+import {
+  useCreateBusinessConfigMutation,
+  useGetBusinessConfigAllQuery,
+  useUpdateBusinessConfigMutation,
+} from "@/redux/services/businessConfigApi";
+import { BusinessConfigData, CustomErrorData } from "@/types";
+import { translateError } from "@/utils/translateError";
+import { toast } from "sonner";
 
 export const useBussinessConfig = () => {
   const {
@@ -9,4 +16,82 @@ export const useBussinessConfig = () => {
   } = useGetBusinessConfigAllQuery();
 
   return { dataBusinessConfigAll, error, isLoading, isSuccess };
+};
+
+export const useCreateBusinessConfig = () => {
+  const [createBusinessConfig] = useCreateBusinessConfigMutation();
+
+  const onCreateBusinessConfig = async (input: Partial<BusinessConfigData>) => {
+    const promise = () =>
+      new Promise(async (resolve, reject) => {
+        try {
+          const result = await createBusinessConfig(input);
+          if (result.error && "data" in result.error) {
+            const error = (result.error.data as CustomErrorData).message;
+            const message = translateError(error as string);
+            reject(new Error(message));
+          }
+          if (result.error) {
+            reject(
+              new Error(
+                "Ocurrió un error inesperado, por favor intenta de nuevo",
+              ),
+            );
+          }
+          resolve(result);
+        } catch (error) {
+          reject(error);
+        }
+      });
+
+    toast.promise(promise(), {
+      loading: "Creando...",
+      success: "Configuración de negocio creada",
+      error: (error) => {
+        return error.message;
+      },
+    });
+  };
+
+  return { onCreateBusinessConfig };
+};
+
+export const useUpdateBusinessConfig = () => {
+  const [updateBusinessConfig] = useUpdateBusinessConfigMutation();
+
+  const onUpdateBusinessConfig = async (
+    input: Partial<BusinessConfigData> & { id: string },
+  ) => {
+    const promise = () =>
+      new Promise(async (resolve, reject) => {
+        try {
+          const result = await updateBusinessConfig(input);
+          if (result.error && "data" in result.error) {
+            const error = (result.error.data as CustomErrorData).message;
+            const message = translateError(error as string);
+            reject(new Error(message));
+          }
+          if (result.error) {
+            reject(
+              new Error(
+                "Ocurrió un error inesperado, por favor intenta de nuevo",
+              ),
+            );
+          }
+          resolve(result);
+        } catch (error) {
+          reject(error);
+        }
+      });
+
+    toast.promise(promise(), {
+      loading: "Actualizando...",
+      success: "Configuración de negocio actualizada",
+      error: (error) => {
+        return error.message;
+      },
+    });
+  };
+
+  return { onUpdateBusinessConfig };
 };
