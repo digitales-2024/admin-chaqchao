33410a25a0d69ed8c7b5a1d7595225035f56542c
Diego Rondón <furrtr4532@gmail.com>
2024-09-18 16:44:59 -0500

    feat: configuiracion de las tablas vista

--- src/app/(admin)/bussiness/classes-configuration/page.tsx
+++ src/app/(admin)/bussiness/classes-configuration/page.tsx
@@ -1,10 +1,12 @@
+"use client";
 import React from "react";
 
+import ClassConfigurationTabs from "@/components/classes-config/ClassConfigurationTabs";
 import { TitleSecction } from "@/components/common/text/TitleSecction";
 
 const HeaderPage = () => (
   <div className="mb-6">
-    <TitleSecction text="Configuración de la Clases" />
+    <TitleSecction text="Configuración de las Clases" />
     <span className="text-sm text-slate-600">
       Complete la información de las clases.
     </span>
@@ -13,8 +15,11 @@ const HeaderPage = () => (
 
 export default function ClassesConfigurationPage() {
   return (
-    <div>
+    <div className="container mx-auto py-10">
       <HeaderPage />
+      <div className="flex items-start space-x-8">
+        <ClassConfigurationTabs />
+      </div>
     </div>
   );
 }
