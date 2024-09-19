eb068403f37eaefccf439d656d04f46fee654d02
Acide <143274747+AcideTI@users.noreply.github.com>
2024-09-19 16:48:14 -0500

    feat: agregar función para verificar el token del usuario

---
+++ src/lib/jwt/verifyUserToken.ts
@@ -0,0 +1,32 @@
+import { User } from "@/types";
+
+// Interfaz para la respuesta de la API
+interface ApiResponse {
+  user: User;
+  [key: string]: any; // Para otros campos adicionales
+}
+// Función para llamar a la API y verificar el token y el estado del usuario
+export async function verifyUserToken(
+  token: string,
+): Promise<{ setCookieHeader: string | null; data: ApiResponse }> {
+  const response = await fetch(
+    process.env["NEXT_PUBLIC_BACKEND_URL"] + "/auth/refresh-token",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Cookie: `token=${token}`,
+      },
+      credentials: "include",
+    },
+  );
+
+  if (!response.ok) {
+    throw new Error("Token inválido o usuario no autorizado");
+  }
+
+  const setCookieHeader = response.headers.get("set-cookie");
+  const data: ApiResponse = await response.json();
+
+  return { setCookieHeader, data };
+}
