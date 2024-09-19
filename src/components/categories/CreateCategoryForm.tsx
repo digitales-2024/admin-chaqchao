eff345ab5e900ae93644e310e0f4670216329997
unknown <francogt8000@gmail.com>
2024-09-19 17:32:00 +0200

    feat(pages): add categories createform

---
+++ src/components/categories/CreateCategoryForm.tsx
@@ -0,0 +1,69 @@
+"use client";
+import { UseFormReturn } from "react-hook-form";
+import { Input } from "@/components/ui/input";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { createCategoriesSchema } from "@/schemas";
+
+interface CreateCategoryFormProps
+  extends Omit<React.ComponentPropsWithRef<"form">, "onSubmit"> {
+  children: React.ReactNode;
+  form: UseFormReturn<createCategoriesSchema>;
+  onSubmit: (data: createCategoriesSchema) => void;
+}
+
+export const CreateCategoryForm = ({
+  children,
+  form,
+  onSubmit,
+}: CreateCategoryFormProps) => {
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <div className="flex flex-col gap-6 p-4 sm:p-0">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel htmlFor="name">Nombre de la categoría</FormLabel>
+                <FormControl>
+                  <Input
+                    id="name"
+                    placeholder="Por ejemplo: Bebidas"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel htmlFor="description">Descripción</FormLabel>
+                <FormControl>
+                  <Input
+                    id="description"
+                    placeholder="Descripción breve de la categoría"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        {children}
+      </form>
+    </Form>
+  );
+};
