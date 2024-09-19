bec69bdce43a537a847d159984c7e65d7d3a895b
Acide <143274747+AcideTI@users.noreply.github.com>
2024-09-19 16:57:54 -0500

    feat: agregamso la decodificacion y la verificacion del token

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,6 +4,7 @@ import type { NextRequest } from "next/server";
 import { decodeToken } from "./lib/jwt/decodeToken";
 import { getToken } from "./lib/jwt/getToken";
 import { redirectToLogin } from "./lib/jwt/redirectToLogin";
+import { verifyUserToken } from "./lib/jwt/verifyUserToken";
 
 const routesNotRequiringAuth = ["/sign-in", "/update-password"];
 
@@ -12,14 +13,17 @@ export async function middleware(request: NextRequest) {
   const token = getToken(request);
   console.log("🚀 ~ middleware ~ token:", token);
 
-  // Decodificar el token
-  const decodedToken = token ? decodeToken(token) : null;
-  console.log("🚀 ~ middleware ~ decodedToken:", decodedToken);
-
   // Si no hay cookie, redirigimos al usuario a la página de login
   if (!token && !routesNotRequiringAuth.includes(request.nextUrl.pathname)) {
     return redirectToLogin(request);
   }
+
+  // Decodificar el token
+  const decodedToken = token ? decodeToken(token) : null;
+  console.log("🚀 ~ middleware ~ decodedToken:", decodedToken);
+
+  const verify = await verifyUserToken(token || "");
+  console.log("🚀 ~ middleware ~ verify:", verify);
 
   // Si hay cookie, redirigimos al usuario a la página de inicio
   if (token && routesNotRequiringAuth.includes(request.nextUrl.pathname)) {
