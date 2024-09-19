2cf2f2c05f4bc1f4b01dc491a37b9951a7a0da37
unknown <francogt8000@gmail.com>
2024-09-19 18:05:16 +0200

    feat(pages): add categories indexschema

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -2,3 +2,8 @@ export { authSchema } from "./authSchema";
 export { updatePasswordSchema } from "./updatePasswordSchema";
 export { updateInfoSchema } from "./account/updateInfoSchema";
 export { type createUsersSchema, usersSchema } from "./users/createUsersSchema";
+export {
+  type createCategoriesSchema,
+  categoriesSchema,
+} from "./categories/createCategoriesSchema";
+export { type updateCategoriesSchema } from "./categories/updateCategoriesSchema";
