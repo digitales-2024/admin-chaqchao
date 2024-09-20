64fcc28a6d66568623df266d0a8a92cc5b151806
Acide <143274747+AcideTI@users.noreply.github.com>
2024-09-20 17:22:43 -0500

    feat: si el token a expirado entonces nos redirige a login

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,35 +4,46 @@ import type { NextRequest } from "next/server";
 import { decodeToken } from "./lib/jwt/decodeToken";
 import { getToken } from "./lib/jwt/getToken";
 import { redirectToLogin } from "./lib/jwt/redirectToLogin";
-import { verifyUserToken } from "./lib/jwt/verifyUserToken";
 
 const routesNotRequiringAuth = ["/sign-in", "/update-password"];
 
 export async function middleware(request: NextRequest) {
   // Extraer la cookie llamada 'access_token'
   const token = getToken(request);
-  console.log("🚀 ~ middleware ~ token:", token);
 
-  // Si no hay cookie, redirigimos al usuario a la página de login
-  if (!token && !routesNotRequiringAuth.includes(request.nextUrl.pathname)) {
-    return redirectToLogin(request);
+  try {
+    // Si no hay cookie, redirigimos al usuario a la página de login
+    if (!token && !routesNotRequiringAuth.includes(request.nextUrl.pathname)) {
+      return redirectToLogin(request);
+    }
+
+    // Decodificar el token
+    const decodedToken = token ? decodeToken(token) : null;
+
+    // Verificamos si el token expiro, mandamos un error si es así
+    if (decodedToken && decodedToken.exp) {
+      const currentTime = Math.floor(Date.now() / 1000);
+      if (decodedToken.exp < currentTime) {
+        throw new Error("TokenExpiredError");
+      }
+    }
+
+    // Si hay cookie, redirigimos al usuario a la página de inicio
+    if (token && routesNotRequiringAuth.includes(request.nextUrl.pathname)) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+
+    const response = NextResponse.next();
+
+    return response;
+  } catch (error) {
+    // Si el error es de token expirado, redirigimos al usuario a la página de login y limpiamos la cookie
+    if ((error as Error)?.message) {
+      const response = redirectToLogin(request);
+      response.cookies.delete("access_token");
+      return response;
+    }
   }
-
-  // Decodificar el token
-  const decodedToken = token ? decodeToken(token) : null;
-  console.log("🚀 ~ middleware ~ decodedToken:", decodedToken);
-
-  const verify = await verifyUserToken(token || "");
-  console.log("🚀 ~ middleware ~ verify:", verify);
-
-  // Si hay cookie, redirigimos al usuario a la página de inicio
-  if (token && routesNotRequiringAuth.includes(request.nextUrl.pathname)) {
-    return NextResponse.redirect(new URL("/", request.url));
-  }
-
-  const response = NextResponse.next();
-
-  return response;
 }
 
 export const config = {
