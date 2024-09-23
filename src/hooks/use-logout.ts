66f5a438a815f249b1e4b7bc9a8c5dee09849620
EleoRomario <eleo.romario@gmail.com>
2024-09-22 22:44:33 -0500

    feat: actualizar hook useLogout para manejar errores de cierre de sesión

--- src/hooks/use-logout.ts
+++ src/hooks/use-logout.ts
@@ -1,4 +1,6 @@
 import { useLogoutMutation } from "@/redux/services/authApi";
+import { CustomErrorData } from "@/types";
+import { translateError } from "@/utils/translateError";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import { toast } from "sonner";
@@ -15,7 +17,22 @@ export const useLogout = () => {
       new Promise(async (resolve, reject) => {
         try {
           const result = await logout();
-          resolve(result);
+          if (
+            result.error &&
+            typeof result.error === "object" &&
+            "data" in result.error
+          ) {
+            const error = (result.error.data as CustomErrorData).message;
+            const message = translateError(error as string);
+            reject(new Error(message));
+          }
+          if (result.error) {
+            reject(
+              new Error(
+                "Ocurrió un error inesperado, por favor intenta de nuevo",
+              ),
+            );
+          }
         } catch (error) {
           reject(error);
         }
@@ -24,7 +41,9 @@ export const useLogout = () => {
     toast.promise(promise(), {
       loading: "Cerrando sesión...",
       success: "Sesión cerrada correctamente",
-      error: "Ocurrió un error al cerrar la sesión",
+      error: (error) => {
+        return error.message;
+      },
     });
   };
 
