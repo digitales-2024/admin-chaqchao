f34f247342b9a6455eff146ab7559250c9bf45d4
Diego Rondón <furrtr4532@gmail.com>
2024-09-25 15:17:33 -0500

    feat: mejorar validación del precio en createProductsSchema

--- src/schemas/products/createProductsSchema.ts
+++ src/schemas/products/createProductsSchema.ts
@@ -5,9 +5,13 @@ export const productsSchema = z.object({
     .string()
     .min(2, { message: "El nombre debe tener al menos 2 caracteres" }),
   description: z.string().optional(),
-  price: z.number().min(1, {
-    message: "El precio es obligatorio y debe ser mayor que 0",
-  }),
+  price: z
+    .number({
+      required_error: "El precio es obligatorio y debe ser mayor que 0",
+    })
+    .min(1, {
+      message: "El precio debe ser mayor que 0",
+    }),
   image: z.string().optional(),
   categoryId: z.string().min(1, { message: "Debes seleccionar una categoría" }),
 });
