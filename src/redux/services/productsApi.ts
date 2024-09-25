cf545d6afdaea03158ac43bfff24ec975e3c5b4a
Diego Rondón <furrtr4532@gmail.com>
2024-09-25 12:18:04 -0500

    feat: agregar servicio productsApi para gestionar productos en Redux

---
+++ src/redux/services/productsApi.ts
@@ -0,0 +1,100 @@
+import { ProductData } from "@/types";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const productsApi = createApi({
+  reducerPath: "productsApi",
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_BACKEND_URL }),
+  tagTypes: ["Product"],
+  endpoints: (build) => ({
+    // Crear un nuevo producto
+    createProduct: build.mutation<ProductData, Partial<ProductData>>({
+      query: (body) => ({
+        url: "/products",
+        method: "POST",
+        body,
+        credentials: "include",
+      }),
+      invalidatesTags: ["Product"],
+    }),
+    // Actualizar un producto por id
+    updateProduct: build.mutation<
+      ProductData,
+      Partial<ProductData> & { id: string }
+    >({
+      query: ({ id, ...body }) => ({
+        url: `/products/${id}`,
+        method: "PATCH",
+        body,
+        credentials: "include",
+      }),
+      invalidatesTags: ["Product"],
+    }),
+    // Obtener un producto por id
+    getProductById: build.query<ProductData, string>({
+      query: (id) => ({
+        url: `/products/${id}`,
+        method: "GET",
+        credentials: "include",
+      }),
+      providesTags: (result, error, id) => [{ type: "Product", id }],
+    }),
+    // Obtener todos los productos
+    getAllProducts: build.query<ProductData[], void>({
+      query: () => ({
+        url: "/products",
+        method: "GET",
+        credentials: "include",
+      }),
+      providesTags: ["Product"],
+    }),
+    // Eliminar un producto por id
+    deleteProduct: build.mutation<{ success: boolean }, { id: string }>({
+      query: ({ id }) => ({
+        url: `/products/${id}`,
+        method: "DELETE",
+        credentials: "include",
+      }),
+      invalidatesTags: ["Product"],
+    }),
+    // Eliminar varios productos
+    deleteProducts: build.mutation<void, { ids: string[] }>({
+      query: (ids) => ({
+        url: "products/remove/all",
+        method: "DELETE",
+        body: ids,
+        credentials: "include",
+      }),
+      invalidatesTags: ["Product"],
+    }),
+    // Activar o desactivar un producto
+    toggleProductActivation: build.mutation<ProductData, { id: string }>({
+      query: ({ id }) => ({
+        url: `/products/toggleactivation/${id}`,
+        method: "PATCH",
+        credentials: "include",
+      }),
+      invalidatesTags: ["Product"],
+    }),
+    // Reactivar varios productos
+    reactivateProducts: build.mutation<void, { ids: string[] }>({
+      query: (ids) => ({
+        url: "products/reactivate/all",
+        method: "PATCH",
+        body: ids,
+        credentials: "include",
+      }),
+      invalidatesTags: ["Product"],
+    }),
+  }),
+});
+
+export const {
+  useCreateProductMutation,
+  useUpdateProductMutation,
+  useGetProductByIdQuery,
+  useGetAllProductsQuery,
+  useDeleteProductMutation,
+  useToggleProductActivationMutation,
+  useReactivateProductsMutation,
+  useDeleteProductsMutation,
+} = productsApi;
