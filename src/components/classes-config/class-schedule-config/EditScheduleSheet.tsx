6a51a5dc6546868c7d0e6187d2a8158d107e683b
EleoRomario <eleo.romario@gmail.com>
2024-09-26 11:54:52 -0500

    refactor: mejorar diseño de la hoja de actualización de horarios de negocio

--- src/components/classes-config/class-schedule-config/EditScheduleSheet.tsx
+++ src/components/classes-config/class-schedule-config/EditScheduleSheet.tsx
@@ -9,9 +9,10 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useEffect } from "react";
 import { useForm, Controller, FormProvider } from "react-hook-form";
 
+import { InputTime } from "@/components/common/input/InputTime";
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import {
   Sheet,
   SheetContent,
@@ -79,46 +80,46 @@ export function EditScheduleSheet({
 
   return (
     <Sheet open={isOpen} onOpenChange={onClose}>
-      <SheetContent side="right">
+      <SheetContent side="right" className="flex flex-col gap-6">
         <SheetHeader>
           <SheetTitle>Editar horario de la Clase</SheetTitle>
           <SheetDescription>
             Modifica el horario de la clase a continuación.
           </SheetDescription>
         </SheetHeader>
-        <FormProvider {...methods}>
-          <form onSubmit={handleSubmit(onSubmit)} className="mt-4 space-y-4">
-            <div className="space-y-4">
-              {/* Input for start time */}
-              <div className="space-y-2">
-                <Label htmlFor="startTime">Hora de Inicio</Label>
-                <Controller
-                  name="startTime"
-                  control={control}
-                  render={({ field }) => (
-                    <Input
-                      id="startTime"
-                      type="time"
-                      value={field.value}
-                      onChange={(e) => field.onChange(e.target.value)}
-                    />
+        <ScrollArea className="w-full gap-4 rounded-md border p-4">
+          <FormProvider {...methods}>
+            <form
+              onSubmit={handleSubmit(onSubmit)}
+              className="mt-4 space-y-4 p-4"
+            >
+              <div className="space-y-4">
+                {/* Input for start time */}
+                <div className="flex flex-col items-center justify-between space-y-2 sm:flex-row">
+                  <Label htmlFor="startTime">Hora de Inicio</Label>
+                  <Controller
+                    name="startTime"
+                    control={control}
+                    render={({ field }) => <InputTime {...field} />}
+                  />
+                  {errors.startTime && (
+                    <p className="text-sm text-red-500">
+                      {errors.startTime.message}
+                    </p>
                   )}
-                />
-                {errors.startTime && (
-                  <p className="text-sm text-red-500">
-                    {errors.startTime.message}
-                  </p>
-                )}
+                </div>
               </div>
-            </div>
-            <SheetFooter className="mt-6 flex justify-end space-x-2">
-              <Button type="button" variant="outline" onClick={onClose}>
-                Cancelar
-              </Button>
-              <Button type="submit">Actualizar Horario</Button>
-            </SheetFooter>
-          </form>
-        </FormProvider>
+              <SheetFooter>
+                <div className="flex flex-row-reverse flex-wrap gap-2">
+                  <Button type="submit">Actualizar Horario</Button>
+                  <Button type="button" variant="outline" onClick={onClose}>
+                    Cancelar
+                  </Button>
+                </div>
+              </SheetFooter>
+            </form>
+          </FormProvider>
+        </ScrollArea>
       </SheetContent>
     </Sheet>
   );
