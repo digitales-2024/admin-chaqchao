f0404010ecd49dcbcda51c5f1dc7b9b84d601a7c
Diego Rondón <furrtr4532@gmail.com>
2024-09-26 09:39:08 -0500

    feat: agregar soporte para vista en móvil en ProductImageDialog y personalizar color de categoría

--- src/components/products/ProductImageDialog.tsx
+++ src/components/products/ProductImageDialog.tsx
@@ -1,80 +1,118 @@
 "use client";
 
+import { useMediaQuery } from "@/hooks/use-media-query";
 import { ProductData } from "@/types";
+import { DialogDescription, DialogTitle } from "@radix-ui/react-dialog";
 import { CheckCircle, XCircle } from "lucide-react";
 import Image from "next/image";
 import { useState } from "react";
 
 import { Badge } from "@/components/ui/badge";
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import {
+  Drawer,
+  DrawerContent,
+  DrawerDescription,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer"; // Asegúrate de importar correctamente el Drawer
 
 interface ProductImageDialogProps {
   imageUrl: string;
   children: React.ReactNode;
   product: ProductData;
+  color: string;
 }
 
 export function ProductImageDialog({
   imageUrl,
   children,
   product,
+  color,
 }: ProductImageDialogProps) {
   const [open, setOpen] = useState(false);
+  const isDesktop = useMediaQuery("(min-width: 640px)");
+
+  const content = (
+    <div className="flex flex-col sm:flex-row sm:items-start">
+      {/* Sección de Imagen: Alineación a la izquierda */}
+      <div className="flex w-full sm:justify-start">
+        <Image
+          src={imageUrl}
+          alt={product.name}
+          width={400}
+          height={400}
+          className="transform rounded-md object-cover transition-transform duration-300 hover:scale-105"
+        />
+      </div>
+
+      {/* Sección de Información */}
+      <div className="mt-6 sm:ml-6 sm:w-2/3">
+        {/* Nombre del Producto */}
+        <h1 className="mb-2 text-2xl font-bold">{product.name}</h1>
+
+        {/* Categoría */}
+        <Badge
+          variant="default"
+          className="mb-4 capitalize"
+          style={{ backgroundColor: color }}
+        >
+          {product.category.name}
+        </Badge>
+
+        {/* Descripción */}
+        <p className="mb-4 text-gray-700">{product.description}</p>
+
+        {/* Precio */}
+        <div className="mb-4 text-2xl font-semibold text-emerald-600">
+          {new Intl.NumberFormat("es-PE", {
+            style: "currency",
+            currency: "PEN",
+          }).format(product.price)}
+        </div>
+
+        {/* Disponibilidad */}
+        <div className="mb-4 flex items-center">
+          {product.isAvailable ? (
+            <>
+              <CheckCircle className="mr-2 text-emerald-500" />
+              <span className="text-emerald-500">Disponible</span>
+            </>
+          ) : (
+            <>
+              <XCircle className="mr-2 text-red-500" />
+              <span className="text-red-500">No Disponible</span>
+            </>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+
+  if (isDesktop)
+    return (
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogTrigger asChild>{children}</DialogTrigger>
+        <DialogTitle></DialogTitle>
+        <DialogDescription></DialogDescription>
+        <DialogContent className="max-w-3xl shadow-none">
+          {content}
+        </DialogContent>
+      </Dialog>
+    );
 
   return (
-    <Dialog open={open} onOpenChange={setOpen}>
-      <DialogTrigger asChild>{children}</DialogTrigger>
-      <DialogContent className="max-w-4xl shadow-none">
-        <div className="mx-auto flex flex-col p-6 sm:flex-row sm:items-start">
-          {/* Sección de Imagen: Alineación a la izquierda */}
-          <div className="flex w-full items-center justify-center sm:w-1/3 sm:justify-start">
-            <Image
-              src={imageUrl}
-              alt={product.name}
-              width={400}
-              height={400}
-              className="transform rounded-md object-cover transition-transform duration-300 hover:scale-105"
-            />
-          </div>
+    <Drawer open={open} onOpenChange={setOpen}>
+      <DrawerTrigger asChild>{children}</DrawerTrigger>
 
-          {/* Sección de Información */}
-          <div className="mt-6 sm:ml-6 sm:mt-0 sm:w-2/3">
-            {/* Nombre del Producto */}
-            <h1 className="mb-2 text-2xl font-bold">{product.name}</h1>
-
-            {/* Categoría */}
-            <Badge variant="outline" className="mb-4 capitalize">
-              {product.category.name}
-            </Badge>
-
-            {/* Descripción */}
-            <p className="mb-4 text-gray-700">{product.description}</p>
-
-            {/* Precio */}
-            <div className="mb-4 text-2xl font-semibold text-emerald-600">
-              {new Intl.NumberFormat("es-PE", {
-                style: "currency",
-                currency: "PEN",
-              }).format(product.price)}
-            </div>
-
-            {/* Disponibilidad */}
-            <div className="mb-4 flex items-center">
-              {product.isAvailable ? (
-                <>
-                  <CheckCircle className="mr-2 text-emerald-500" />
-                  <span className="text-emerald-500">Disponible</span>
-                </>
-              ) : (
-                <>
-                  <XCircle className="mr-2 text-red-500" />
-                  <span className="text-red-500">No Disponible</span>
-                </>
-              )}
-            </div>
-          </div>
-        </div>
-      </DialogContent>
-    </Dialog>
+      <DrawerContent className="flex flex-col items-center justify-center">
+        <DrawerHeader>
+          <DrawerTitle></DrawerTitle>
+          <DrawerDescription></DrawerDescription>
+        </DrawerHeader>
+        {content}
+      </DrawerContent>
+    </Drawer>
   );
 }
