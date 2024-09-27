9c139eab9b559ea1f91938c50a9067ac2b33bb16
Diego Rondón <furrtr4532@gmail.com>
2024-09-27 08:52:03 -0500

    feat: agregar funcionalidad de activación de productos y mejorar diseño en ProductImageDialog

--- src/components/products/ProductImageDialog.tsx
+++ src/components/products/ProductImageDialog.tsx
@@ -1,9 +1,10 @@
 "use client";
 
 import { useMediaQuery } from "@/hooks/use-media-query";
+import { useToggleProductActivation } from "@/hooks/use-products";
 import { ProductData } from "@/types";
 import { DialogTitle as UIDialogTitle } from "@radix-ui/react-dialog";
-import { PackageCheck, PackageX } from "lucide-react";
+import { PackageCheck, PackageX, ShieldAlert, ShieldMinus } from "lucide-react";
 import Image from "next/image";
 import { useState } from "react";
 
@@ -27,42 +28,53 @@ import { Separator } from "@/components/ui/separator";
 
 import { cn } from "@/lib/utils";
 
+import { ScrollArea } from "../ui/scroll-area";
 import { Switch } from "../ui/switch";
 
 interface ProductImageDialogProps {
   imageUrl: string;
   children: React.ReactNode;
   product: ProductData;
-  color: string;
+  borderColor: string;
 }
 
 export function ProductImageDialog({
   imageUrl,
   children,
   product,
-  color,
+  borderColor,
 }: ProductImageDialogProps) {
   const [open, setOpen] = useState(false);
   const isDesktop = useMediaQuery("(min-width: 640px)");
 
+  const { onToggleProductActivation } = useToggleProductActivation();
+  const [isAvailable, setIsAvailable] = useState(product.isAvailable);
+
+  const handleToggle = async (checked: boolean) => {
+    const productId = product.id;
+    await onToggleProductActivation(productId);
+    setIsAvailable(checked);
+  };
+
   const content = (
-    <div className="grid gap-6 p-5 py-7 lg:grid-cols-[400px_1fr]">
+    <div className="flex snap-center flex-col place-items-center gap-6 py-7 sm:flex-row sm:place-items-start">
       {/* Imagen y detalles básicos */}
       <div className="space-y-4">
-        <div className="relative aspect-square h-auto w-full">
+        <div className="relative flex h-auto w-full">
           <Image
             height={400}
             width={400}
             src={imageUrl}
             alt={product.name}
             objectFit="cover"
+            className="flex-1"
           />
         </div>
       </div>
-      {/* Información detallada y formulario de edición */}
+      {/* Información detallada */}
       <div className="space-y-6">
         <Card className="border-slate-50">
-          <CardContent className="flex flex-col gap-10 p-10">
+          <CardContent className="flex flex-col gap-6 p-10">
             <div className="space-y-2">
               <Label htmlFor="description">Descripción</Label>
               <p className="text-balance text-sm font-light text-gray-400">
@@ -73,9 +85,9 @@ export function ProductImageDialog({
               <Label htmlFor="category">Categoría</Label>
               <div className="mb-2 flex items-center">
                 <Badge
-                  variant="default"
+                  variant="outline"
                   className="capitalize"
-                  style={{ backgroundColor: color }}
+                  style={{ borderColor }}
                 >
                   {product.category.name}
                 </Badge>
@@ -83,7 +95,7 @@ export function ProductImageDialog({
             </div>
             <div className="space-y-2">
               <Label htmlFor="price">Precio (PEN)</Label>
-              <p>
+              <p className="text-balance text-lg font-semibold text-emerald-500">
                 {new Intl.NumberFormat("es-PE", {
                   style: "currency",
                   currency: "PEN",
@@ -93,15 +105,20 @@ export function ProductImageDialog({
             <div className="space-y-2">
               <Label htmlFor="stock">Disponibilidad</Label>
               <div className="flex items-center gap-2">
-                <Switch checked={product.isAvailable} />
+                <Switch
+                  checked={isAvailable}
+                  onCheckedChange={handleToggle}
+                  onClick={(e) => e.stopPropagation()}
+                  className="translate-y-0.5"
+                />
                 <span
                   className={cn(
-                    "text-xs text-emerald-500",
-                    product.isAvailable ? "text-emerald-500" : "text-slate-500",
+                    "text-xs",
+                    isAvailable ? "text-emerald-500" : "text-slate-500",
                   )}
                 >
-                  {product.isAvailable ? (
-                    <span className="inline-flex gap-2">
+                  {isAvailable ? (
+                    <span className="inline-flex gap-2 align-bottom">
                       <PackageCheck size={16} className="flex-shrink-0" />{" "}
                       Disponible
                     </span>
@@ -114,48 +131,76 @@ export function ProductImageDialog({
                 </span>
               </div>
             </div>
+            <div className="space-y-2">
+              <Label htmlFor="stock">Restringido</Label>
+              <div className="flex items-center gap-2">
+                <span
+                  className={cn(
+                    "text-xs",
+                    product.isRestricted ? "text-orange-500" : "text-slate-500",
+                  )}
+                >
+                  {product.isRestricted ? (
+                    <span className="inline-flex gap-2 align-bottom">
+                      <ShieldAlert size={16} className="flex-shrink-0" />{" "}
+                      Retrincción de Edad
+                    </span>
+                  ) : (
+                    <span className="inline-flex gap-2">
+                      <ShieldMinus size={16} className="flex-shrink-0" /> Sin
+                      Retrincción de Edad
+                    </span>
+                  )}
+                </span>
+              </div>
+            </div>
+            <div className="space-y-2">
+              <Label>Fecha de creación</Label>
+              <p className="text-balance text-sm font-light text-gray-400">
+                {new Date(product.createdAt).toLocaleString()}
+              </p>
+            </div>
+            <div className="space-y-2">
+              <Label>Fecha de actualización</Label>
+              <p className="text-balance text-sm font-light text-gray-400">
+                {new Date(product.updatedAt).toLocaleString()}
+              </p>
+            </div>
           </CardContent>
         </Card>
       </div>
     </div>
   );
 
-  const dialogContent = (
-    <DialogContent className="max-h-lvh max-w-4xl overflow-y-auto p-10">
-      <DialogHeader>
-        <UIDialogTitle className="text-2xl font-bold uppercase">
-          {product.name}
-        </UIDialogTitle>
-        <Separator className="my-4" />
-      </DialogHeader>
-      {content}
-    </DialogContent>
-  );
-
-  const drawerContent = (
-    <DrawerContent className="flex flex-col overflow-y-auto p-4">
-      <DrawerHeader>
-        <UIDrawerTitle className="text-2xl font-bold uppercase">
-          {product.name}
-        </UIDrawerTitle>
-        <Separator className="my-4" />
-      </DrawerHeader>
-      {content}
-    </DrawerContent>
-  );
-
   if (isDesktop)
     return (
       <Dialog open={open} onOpenChange={setOpen}>
         <DialogTrigger asChild>{children}</DialogTrigger>
-        {dialogContent}
+        <DialogContent className="max-w-2xl justify-start p-10 lg:max-w-4xl">
+          <DialogHeader>
+            <UIDialogTitle className="text-2xl font-bold uppercase">
+              {product.name}
+            </UIDialogTitle>
+            <Separator className="my-4" />
+          </DialogHeader>
+          <ScrollArea>{content}</ScrollArea>
+        </DialogContent>
       </Dialog>
     );
 
   return (
     <Drawer open={open} onOpenChange={setOpen}>
       <DrawerTrigger asChild>{children}</DrawerTrigger>
-      {drawerContent}
+      <DrawerContent className="h-4/5">
+        <DrawerHeader>
+          <UIDrawerTitle className="text-2xl font-bold uppercase">
+            {product.name}
+          </UIDrawerTitle>
+          <Separator className="my-4" />
+        </DrawerHeader>
+
+        <ScrollArea>{content}</ScrollArea>
+      </DrawerContent>
     </Drawer>
   );
 }
