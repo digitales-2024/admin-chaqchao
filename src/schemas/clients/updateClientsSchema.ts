ecb19b3e03399fc87bb3adbbec1aae48d6bdc3c7
EleoRomario <eleo.romario@gmail.com>
2024-09-30 11:04:30 -0500

    feat: agregar validación de número de teléfono en el esquema de clientes

--- src/schemas/clients/updateClientsSchema.ts
+++ src/schemas/clients/updateClientsSchema.ts
@@ -1,3 +1,4 @@
+import { isValidPhoneNumber } from "react-phone-number-input";
 import * as z from "zod";
 
 // Esquema base para clientes
@@ -5,7 +6,9 @@ export const clientsSchema = z.object({
   name: z.string().min(2, {
     message: "El nombre del cliente debe tener al menos 2 caracteres",
   }),
-  phone: z.string().min(1, { message: "El teléfono es obligatorio" }),
+  phone: z
+    .string()
+    .refine(isValidPhoneNumber, { message: "Número de teléfono no válido" }),
   birthDate: z.string().regex(/^\d{4}-\d{2}-\d{2}$/, {
     message: "Fecha de nacimiento no válida. Use el formato YYYY-MM-DD",
   }),
