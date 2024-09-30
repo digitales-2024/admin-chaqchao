23e10579b6b86d03216a30317d2360a63a2f76b1
FrancoGT <francogt8000@gmail.com>
2024-10-01 00:26:42 +0200

    feat: componente input date

---
+++ src/components/ui/date-input.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { format } from "date-fns";
+import { es } from "date-fns/locale";
+import { Calendar as CalendarIcon } from "lucide-react";
+import * as React from "react";
+
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
+import { cn } from "@/lib/utils";
+
+interface DateInputProps {
+  value: Date | undefined;
+  onChange: (date: Date | undefined) => void;
+}
+
+export function DateInput({ value, onChange }: DateInputProps) {
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button
+          variant={"outline"}
+          className={cn(
+            "w-[280px] justify-start text-left font-normal",
+            !value && "text-muted-foreground",
+          )}
+        >
+          <CalendarIcon className="mr-2 h-4 w-4" />
+          {value ? (
+            format(value, "PPP", { locale: es })
+          ) : (
+            <span>Seleccione una fecha</span>
+          )}
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-auto p-0">
+        <Calendar
+          mode="single"
+          selected={value}
+          onSelect={onChange}
+          initialFocus
+          locale={es} // Establecer el idioma del calendario
+        />
+      </PopoverContent>
+    </Popover>
+  );
+}
