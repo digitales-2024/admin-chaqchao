aed785c2cf83b040b316e1b2263e6427d721da57
FrancoGT <francogt8000@gmail.com>
2024-10-01 14:49:57 -0500

    feat: cambio de tipo en el campo de fecha de nacimiento

--- src/types/client.ts
+++ src/types/client.ts
@@ -4,7 +4,7 @@ export type Client = {
   email: string; // Correo electrónico del cliente
   phone: string; // Teléfono del cliente
   password: string; // Contraseña del cliente
-  birthDate: string; // Fecha de nacimiento (timestamp)
+  birthDate: Date; // Fecha de nacimiento (timestamp)
   isGoogleAuth: boolean; // Indica si el cliente se autenticó con Google
   token: string; // Token de autenticación
   lastLogin: string; // Último inicio de sesión (timestamp con zona horaria)
