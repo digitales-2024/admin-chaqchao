2b9ec4ce07336e38c903d19c058add7c64a21cc1
FrancoGT <francogt8000@gmail.com>
2024-10-01 14:44:00 -0500

    feat: ajustes en el panel para ver hora de conexion y columnas en castellano

--- src/components/clients/ClientsTableColumns.tsx
+++ src/components/clients/ClientsTableColumns.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+import { useProfile } from "@/hooks/use-profile";
 import { Client } from "@/types";
 import { type ColumnDef } from "@tanstack/react-table";
-import { format } from "date-fns";
-import { Ellipsis, Trash } from "lucide-react";
+import { parseISO, format } from "date-fns";
+import { es } from "date-fns/locale";
+import { Ellipsis, Trash, RefreshCcwDot } from "lucide-react"; // Agrega RefreshCcwDot para la reactivación
 import { useState } from "react";
 
 import { Button } from "@/components/ui/button";
@@ -19,59 +21,75 @@ import {
 import { DataTableColumnHeader } from "../data-table/DataTableColumnHeader";
 import { Badge } from "../ui/badge";
 import { DesactivateClientDialog } from "./DesactivateClientDialog";
+import { ReactivateClientDialog } from "./ReactivateClientDialog"; // Asegúrate de importar el diálogo para reactivar
 import { UpdateClientSheet } from "./UpdateClientSheet";
 
 export const clientsColumns = (): ColumnDef<Client>[] => {
   return [
     {
-      id: "name",
+      id: "nombre",
       accessorKey: "name",
       header: ({ column }) => (
         <DataTableColumnHeader column={column} title="Nombre" />
       ),
       cell: ({ row }) => (
-        <div className="truncate capitalize">{row.getValue("name")}</div>
+        <div className="truncate capitalize">{row.getValue("nombre")}</div>
       ),
     },
     {
-      id: "email",
+      id: "correo",
       accessorKey: "email",
       header: ({ column }) => (
         <DataTableColumnHeader column={column} title="Correo" />
       ),
       cell: ({ row }) => (
-        <div className="truncate">{row.getValue("email")}</div>
+        <div className="truncate">{row.getValue("correo")}</div>
       ),
     },
     {
-      id: "phone",
+      id: "teléfono",
       accessorKey: "phone",
       header: ({ column }) => (
         <DataTableColumnHeader column={column} title="Teléfono" />
       ),
       cell: ({ row }) => (
-        <div className="truncate">{row.getValue("phone")}</div>
+        <div className="truncate">{row.getValue("teléfono")}</div>
       ),
     },
     {
-      id: "birthDate",
+      id: "fechaNacimiento",
       accessorKey: "birthDate",
       header: ({ column }) => (
         <DataTableColumnHeader column={column} title="Fecha de nacimiento" />
       ),
-      cell: ({ row }) => (
-        <div>{format(new Date(row.getValue("birthDate")), "yyyy-MM-dd")}</div> // Formatea la fecha correctamente
-      ),
+      cell: ({ row }) => {
+        console.log(row.original.birthDate);
+        return (
+          <div>{format(row.original.birthDate, "PPP", { locale: es })}</div>
+        );
+      },
     },
     {
-      id: "lastLogin",
+      id: "últimoAcceso",
       accessorKey: "lastLogin",
       header: ({ column }) => (
         <DataTableColumnHeader column={column} title="Último acceso" />
       ),
-      cell: ({ row }) => (
-        <div>{format(new Date(row.getValue("lastLogin")), "yyyy-MM-dd")}</div> // Formatea la fecha correctamente
-      ),
+      cell: ({ row }) => {
+        // Verificar si el valor es undefined
+        if (row.getValue("últimoAcceso") === undefined) {
+          return <div>Sin datos</div>;
+        }
+
+        const parsedDate = parseISO(row.getValue("últimoAcceso"));
+
+        // Asegúrate de que parsedDate es un objeto Date válido
+        if (isNaN(parsedDate.getTime())) {
+          return <div>Sin datos</div>;
+        }
+
+        return <div>{format(parsedDate, "yyyy-MM-dd HH:mm:ss")}</div>;
+      },
     },
     {
       id: "estado",
@@ -101,8 +119,10 @@ export const clientsColumns = (): ColumnDef<Client>[] => {
       size: 10,
       cell: function Cell({ row }) {
         const [showEditDialog, setShowEditDialog] = useState(false);
+        const [showReactivateDialog, setShowReactivateDialog] = useState(false); // Añadido para manejar la reactivación
         const [isDialogOpen, setIsDialogOpen] = useState(false);
         const client = row.original;
+        const { user } = useProfile();
 
         return (
           <div>
@@ -110,6 +130,14 @@ export const clientsColumns = (): ColumnDef<Client>[] => {
               open={showEditDialog}
               onOpenChange={setShowEditDialog}
               client={client}
+            />
+            <ReactivateClientDialog // Diálogo para reactivar al cliente
+              client={client}
+              open={showReactivateDialog}
+              onOpenChange={setShowReactivateDialog}
+              onSuccess={() => {
+                row.toggleSelected(false);
+              }}
             />
             <DropdownMenu>
               <DropdownMenuTrigger asChild>
@@ -126,6 +154,17 @@ export const clientsColumns = (): ColumnDef<Client>[] => {
                   Editar
                 </DropdownMenuItem>
                 <DropdownMenuSeparator />
+                {user?.isSuperAdmin && (
+                  <DropdownMenuItem
+                    onSelect={() => setShowReactivateDialog(true)}
+                    disabled={row.original.isActive}
+                  >
+                    Reactivar
+                    <DropdownMenuShortcut>
+                      <RefreshCcwDot className="size-4" aria-hidden="true" />
+                    </DropdownMenuShortcut>
+                  </DropdownMenuItem>
+                )}
                 <DropdownMenuItem onSelect={() => setIsDialogOpen(true)}>
                   Eliminar
                   <DropdownMenuShortcut>
