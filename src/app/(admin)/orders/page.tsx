dc4dadaf2c6af1ea213058eeb34611d09e6d0a2f
EleoRomario <eleo.romario@gmail.com>
2024-10-01 16:17:52 -0500

    feat: agregar datos de pedidos a la tabla de pedidos en la página de pedidos

--- src/app/(admin)/orders/page.tsx
+++ src/app/(admin)/orders/page.tsx
@@ -1,10 +1,41 @@
+"use client";
+import { useOrders } from "@/hooks/use-orders";
+
+import { ErrorPage } from "@/components/common/ErrorPage";
 import { HeaderPage } from "@/components/common/HeaderPage";
 import { Shell } from "@/components/common/Shell";
+import { DataTableSkeleton } from "@/components/data-table/DataTableSkeleton";
 import { FilterDate } from "@/components/orders/FilterDate";
 import { FilterStatus } from "@/components/orders/FilterStatus";
 import { TableOrders } from "@/components/orders/TableOrders";
 
 export default function PagerOrders() {
+  const { dataOrders, isLoadingOrders } = useOrders();
+
+  if (isLoadingOrders) {
+    return (
+      <Shell>
+        <HeaderPage
+          title="Roles"
+          description="Aquí puedes ver la lista de roles registrados en la aplicación."
+        />
+        <DataTableSkeleton columnCount={5} searchableColumnCount={1} />
+      </Shell>
+    );
+  }
+
+  if (!dataOrders) {
+    return (
+      <Shell>
+        <HeaderPage
+          title="Roles"
+          description="Aquí puedes ver la lista de roles registrados en la aplicación."
+        />
+        <ErrorPage />
+      </Shell>
+    );
+  }
+
   return (
     <Shell>
       <div className="flex flex-wrap justify-between gap-4">
@@ -15,7 +46,7 @@ export default function PagerOrders() {
           <FilterDate />
         </div>
       </div>
-      <TableOrders />
+      <TableOrders data={dataOrders} />
     </Shell>
   );
 }
