177c4f7efe5473d6a1ec31564772290e5ede1e05
FrancoGT <francogt8000@gmail.com>
2024-10-01 16:27:46 -0500

    feat: quitando comentarios innecesarios

--- src/schemas/clients/updateClientsSchema.ts
+++ src/schemas/clients/updateClientsSchema.ts
@@ -1,7 +1,6 @@
 import { isValidPhoneNumber } from "react-phone-number-input";
 import * as z from "zod";
 
-// Esquema base para clientes
 export const clientsSchema = z.object({
   name: z.string().min(2, {
     message: "El nombre del cliente debe tener al menos 2 caracteres",
@@ -14,14 +13,8 @@ export const clientsSchema = z.object({
   }),
 });
 
-// Esquema para actualizar clientes (con campos opcionales)
 export const updateClientsSchema = clientsSchema.partial().extend({
-  // Opción 1: Hacer el ID opcional si no siempre lo necesitas
   id: z.string().optional(),
-
-  // Opción 2: Aceptar tanto UUID como cualquier cadena no vacía
-  // id: z.union([z.string().uuid({ message: "ID inválido" }), z.string().min(1, { message: "ID inválido" })]),
 });
 
-// Tipos derivados de los esquemas
 export type UpdateClientsSchema = z.infer<typeof updateClientsSchema>;
