22bc4fd14e038c5934247fb5d1b306ab5bd706b2
EleoRomario <eleo.romario@gmail.com>
2024-10-02 10:16:30 -0500

    feat: agregar servicio de API para manejar pedidos

---
+++ src/redux/services/ordersApi.ts
@@ -0,0 +1,53 @@
+import { Order } from "@/types";
+import { createApi } from "@reduxjs/toolkit/query/react";
+
+import baseQueryWithReauth from "./baseQuery";
+
+export const ordersApi = createApi({
+  reducerPath: "ordersApi",
+  baseQuery: baseQueryWithReauth,
+  tagTypes: ["Orders"],
+  endpoints: (build) => ({
+    updateOrder: build.mutation<Order, Partial<Order> & { id: string }>({
+      query: ({ id, ...body }) => ({
+        url: `/orders/${id}`,
+        method: "PATCH",
+        body,
+        credentials: "include",
+      }),
+      invalidatesTags: ["Orders"],
+    }),
+    getOrderById: build.query<Order, string>({
+      query: (id) => ({
+        url: `/orders/${id}`,
+        method: "GET",
+        credentials: "include",
+      }),
+      providesTags: (result, error, id) => [{ type: "Orders", id }],
+    }),
+    getOrdersAll: build.query<Order[], Date | string>({
+      query: (date) => ({
+        url: "/orders",
+        params: { date },
+        method: "GET",
+        credentials: "include",
+      }),
+      providesTags: ["Orders"],
+    }),
+    deleteOrder: build.mutation<{ success: boolean }, { id: string }>({
+      query: ({ id }) => ({
+        url: `/orders/${id}`,
+        method: "DELETE",
+        credentials: "include",
+      }),
+      invalidatesTags: ["Orders"],
+    }),
+  }),
+});
+
+export const {
+  useUpdateOrderMutation,
+  useGetOrderByIdQuery,
+  useGetOrdersAllQuery,
+  useDeleteOrderMutation,
+} = ordersApi;
