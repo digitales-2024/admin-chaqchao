e0606e8938ffdc59131fa12acf77bf949137828a
EleoRomario <eleo.romario@gmail.com>
2024-10-02 15:15:55 -0500

    feat: actualizar getOrdersAll para aceptar si recibimos el socket de nuevo pedido

--- src/hooks/use-orders.ts
+++ src/hooks/use-orders.ts
@@ -1,6 +1,8 @@
 "use client";
 import { useGetOrdersAllQuery } from "@/redux/services/ordersApi";
 
+import { useSocket } from "./use-socket";
+
 export const useOrders = (dateFilter: Date | string, status?: string) => {
   const date =
     typeof dateFilter === "string" ? dateFilter : dateFilter.toISOString();
@@ -9,11 +11,19 @@ export const useOrders = (dateFilter: Date | string, status?: string) => {
     data: dataOrders,
     isLoading: isLoadingOrders,
     error: errorOrders,
+    refetch: refetchOrders,
   } = useGetOrdersAllQuery({ date, status });
+
+  const { socket } = useSocket();
+
+  socket.on("new-order", () => {
+    refetchOrders();
+  });
 
   return {
     dataOrders,
     isLoadingOrders,
     errorOrders,
+    refetchOrders,
   };
 };
