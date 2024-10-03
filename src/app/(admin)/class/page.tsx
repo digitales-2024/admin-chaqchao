ea6d3b559c0a517f4c62a3b23321449bdc220e0e
Diego Rondón <furrtr4532@gmail.com>
2024-10-03 14:47:31 -0500

    feat: agregar generación de colores basados en la cantidad de lenguajes únicos en la tabla de clases

--- src/app/(admin)/class/page.tsx
+++ src/app/(admin)/class/page.tsx
@@ -1,14 +1,20 @@
 "use client";
 import { useClasses } from "@/hooks/use-class";
+import { format } from "date-fns";
+import { useState } from "react";
 
 import { ClassesTable } from "@/components/class/ClassesTable";
+import { FilterDateClasses } from "@/components/class/FilterDateClasses";
 import { ErrorPage } from "@/components/common/ErrorPage";
 import { HeaderPage } from "@/components/common/HeaderPage";
 import { Shell } from "@/components/common/Shell";
 import { DataTableSkeleton } from "@/components/data-table/DataTableSkeleton";
 
 export default function ClassPage() {
-  const { allDataClasses, isLoadingDataClasses } = useClasses();
+  const [date, setDate] = useState<Date>(new Date());
+  const { allDataClasses, isLoadingDataClasses } = useClasses(
+    format(date, "yyyy-MM-dd"),
+  );
 
   if (isLoadingDataClasses) {
     return (
@@ -36,10 +42,15 @@ export default function ClassPage() {
 
   return (
     <Shell>
-      <HeaderPage
-        title="Clases"
-        description="Aquí puedes ver la lista de clases registradas."
-      />
+      <div className="flex flex-wrap justify-between gap-4">
+        <HeaderPage
+          title="Clases"
+          description="Aquí puedes ver la lista de clases registradas."
+        />
+        <div className="inline-flex flex-wrap justify-end gap-2">
+          <FilterDateClasses date={date} setDate={setDate} />
+        </div>
+      </div>
       <ClassesTable data={allDataClasses} />
     </Shell>
   );
