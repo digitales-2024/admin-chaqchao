3794d2a32196974343b876c2c4be84312c5697d8
EleoRomario <eleo.romario@gmail.com>
2024-10-03 13:27:41 -0500

    feat: actualizar use-orders.ts para mostrar los productos de un pedido en la tabla de pedidos

--- src/hooks/use-orders.ts
+++ src/hooks/use-orders.ts
@@ -1,10 +1,31 @@
 "use client";
-import { useGetOrdersAllQuery } from "@/redux/services/ordersApi";
+import {
+  useGetOrdersAllQuery,
+  useUpdateOrderStatusMutation,
+} from "@/redux/services/ordersApi";
 import { socket } from "@/socket/socket";
+import { CustomErrorData } from "@/types";
+import { translateError } from "@/utils/translateError";
+import { toast } from "sonner";
 
-export const useOrders = (dateFilter: Date | string, status?: string) => {
-  const date =
-    typeof dateFilter === "string" ? dateFilter : dateFilter.toISOString();
+type StatusTranslations = {
+  [key: string]: string;
+};
+
+const OrderStatusEs = {
+  PENDING: "Pendiente",
+  CONFIRMED: "Confirmado",
+  READY: "Listo",
+  COMPLETED: "Completado",
+  CANCELLED: "Cancelado",
+};
+
+export const useOrders = (dateFilter?: Date | string, status?: string) => {
+  const date = dateFilter
+    ? typeof dateFilter === "string"
+      ? dateFilter
+      : dateFilter.toISOString()
+    : new Date().toISOString();
 
   const {
     data: dataOrders,
@@ -17,10 +38,56 @@ export const useOrders = (dateFilter: Date | string, status?: string) => {
     refetchOrders();
   });
 
+  const [
+    updateOrderStatus,
+    { isLoading: isLoadingUpdateOrderStatus, error: errorUpdateOrderStatus },
+  ] = useUpdateOrderStatusMutation();
+
+  const onOrderStatusUpdate = (id: string, status: string) => {
+    const promise = () =>
+      new Promise(async (resolve, reject) => {
+        try {
+          const result = await updateOrderStatus({ id, status });
+          if (
+            result.error &&
+            typeof result.error === "object" &&
+            "data" in result.error
+          ) {
+            const error = (result.error.data as CustomErrorData).message;
+            const message = translateError(error as string);
+            reject(new Error(message));
+          }
+          if (result.error) {
+            reject(
+              new Error(
+                "Ocurrió un error inesperado, por favor intenta de nuevo",
+              ),
+            );
+          }
+          resolve(result);
+        } catch (error) {
+          reject(error);
+        }
+      });
+
+    function translateStatus(status: string) {
+      const translations: StatusTranslations = OrderStatusEs;
+      return translations[status] || status;
+    }
+    return toast.promise(promise(), {
+      loading: "Actualizando estado del pedido...",
+      success: `Estado de pedido a ${translateStatus(status)} actualizado`,
+      error: (err) => err.message,
+    });
+  };
+
   return {
     dataOrders,
     isLoadingOrders,
     errorOrders,
     refetchOrders,
+    onOrderStatusUpdate,
+    isLoadingUpdateOrderStatus,
+    errorUpdateOrderStatus,
   };
 };
