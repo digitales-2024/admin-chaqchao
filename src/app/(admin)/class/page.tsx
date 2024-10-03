d1e5bbe8ec34745d8b56e4500da687fe2016ec23
Diego Rondón <furrtr4532@gmail.com>
2024-10-03 09:02:11 -0500

    feat: implementar página de clases con manejo de carga y errores

--- src/app/(admin)/class/page.tsx
+++ src/app/(admin)/class/page.tsx
@@ -1,5 +1,46 @@
-import React from "react";
+"use client";
+import { useClasses } from "@/hooks/use-class";
+
+import { ClassesTable } from "@/components/class/ClassesTable";
+import { ErrorPage } from "@/components/common/ErrorPage";
+import { HeaderPage } from "@/components/common/HeaderPage";
+import { Shell } from "@/components/common/Shell";
+import { DataTableSkeleton } from "@/components/data-table/DataTableSkeleton";
 
 export default function ClassPage() {
-  return <div></div>;
+  const { allDataClasses, isLoadingDataClasses } = useClasses();
+
+  if (isLoadingDataClasses) {
+    return (
+      <Shell>
+        <HeaderPage
+          title="Clases"
+          description="Aquí puedes ver la lista de clases registradas."
+        />
+        <DataTableSkeleton columnCount={5} searchableColumnCount={1} />
+      </Shell>
+    );
+  }
+
+  if (!allDataClasses) {
+    return (
+      <Shell>
+        <HeaderPage
+          title="Clases"
+          description="Aquí puedes ver la lista de clases registradas."
+        />
+        <ErrorPage />
+      </Shell>
+    );
+  }
+
+  return (
+    <Shell>
+      <HeaderPage
+        title="Clases"
+        description="Aquí puedes ver la lista de clases registradas."
+      />
+      <ClassesTable data={allDataClasses} />
+    </Shell>
+  );
 }
