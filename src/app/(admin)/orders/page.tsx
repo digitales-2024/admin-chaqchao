b7ecb59a51dc8707428cdcf651fdd26ffd3f6e29
EleoRomario <eleo.romario@gmail.com>
2024-10-04 13:13:46 -0500

    feat: agregar componente SwitchModeView para gestionar la vista de pedidos en modo kanban o tabla

--- src/app/(admin)/orders/page.tsx
+++ src/app/(admin)/orders/page.tsx
@@ -1,9 +1,9 @@
 "use client";
+import { useModeViewOrder } from "@/hooks/use-mode-view-order";
 import { useOrders } from "@/hooks/use-orders";
+import { useStore } from "@/hooks/use-store";
 import { OrderStatus } from "@/types";
 import { format } from "date-fns";
-import { motion } from "framer-motion";
-import { BetweenVerticalStart, TableProperties } from "lucide-react";
 import { useState } from "react";
 
 import { ErrorPage } from "@/components/common/ErrorPage";
@@ -13,22 +13,13 @@ import { DataTableSkeleton } from "@/components/data-table/DataTableSkeleton";
 import { FilterDate } from "@/components/orders/FilterDate";
 import { FilterStatus } from "@/components/orders/FilterStatus";
 import { KanbanBoard } from "@/components/orders/kanban/KanbanBoard";
+import { SwitchModeView } from "@/components/orders/SwitchModeView";
 import { TableOrders } from "@/components/orders/TableOrders";
-import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "@/components/ui/tooltip";
-
-const tabs = {
-  table: "Tabla",
-  kanban: "Kanban",
-};
 
 export default function PagerOrders() {
+  const mode = useStore(useModeViewOrder, (state) => state);
+
   const [date, setDate] = useState<Date>(new Date());
   const [filterStatus, setFilterStatus] = useState<OrderStatus>(
     OrderStatus.ALL,
@@ -37,7 +28,6 @@ export default function PagerOrders() {
     format(date, "yyyy-MM-dd"),
     filterStatus,
   );
-  const [activeView, setActiveView] = useState(tabs.table);
 
   if (isLoadingOrders) {
     return (
@@ -57,19 +47,6 @@ export default function PagerOrders() {
     );
   }
 
-  const viewData = [
-    {
-      id: tabs.table,
-      icon: TableProperties,
-      tooltip: "Ver pedidos en formato de tabla",
-    },
-    {
-      id: tabs.kanban,
-      icon: BetweenVerticalStart,
-      tooltip: "Ver pedidos en formato de kanban",
-    },
-  ];
-
   return (
     <Shell>
       <div className="flex flex-wrap justify-between gap-4">
@@ -82,43 +59,10 @@ export default function PagerOrders() {
           <FilterDate date={date} setDate={setDate} />
         </div>
       </div>
-      <div className="flex justify-end">
-        <div className="flex w-fit space-x-1 rounded-xl bg-secondary p-1">
-          {viewData.map(({ id, icon: Icon, tooltip }) => (
-            <Button
-              key={id}
-              variant="ghost"
-              className={`${
-                activeView === id
-                  ? "text-slate-500"
-                  : "text-muted-foreground hover:text-primary"
-              } relative z-10 flex items-center justify-center rounded-lg px-[10px] text-sm font-medium transition focus-visible:outline-2`}
-              onClick={() => setActiveView(id)}
-            >
-              <span className="sr-only">view</span>
-              <TooltipProvider>
-                <Tooltip>
-                  <TooltipTrigger>
-                    <Icon className="size-5" strokeWidth={1.5} />
-                  </TooltipTrigger>
-                  <TooltipContent>{tooltip}</TooltipContent>
-                </Tooltip>
-              </TooltipProvider>
-              {activeView === id && (
-                <motion.span
-                  layoutId="bubble"
-                  className="absolute inset-0 -z-10 bg-white"
-                  style={{ borderRadius: 8 }}
-                  transition={{ type: "spring", bounce: 0.2, duration: 0.6 }}
-                />
-              )}
-            </Button>
-          ))}
-        </div>
-      </div>
+      <SwitchModeView />
       <Separator />
-      {activeView === tabs.table && <TableOrders data={dataOrders} />}
-      {activeView === tabs.kanban && <KanbanBoard data={dataOrders} />}
+      {mode?.modeViewOrder === "table" && <TableOrders data={dataOrders} />}
+      {mode?.modeViewOrder === "kanban" && <KanbanBoard data={dataOrders} />}
     </Shell>
   );
 }
