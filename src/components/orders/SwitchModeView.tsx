c7f520c794e33510ec1d3500b7035b5b0c24e215
EleoRomario <eleo.romario@gmail.com>
2024-10-04 16:05:15 -0500

    feat: agregar funcionalidad para cambiar entre vista de pedidos en modo kanban o tabla

--- src/components/orders/SwitchModeView.tsx
+++ src/components/orders/SwitchModeView.tsx
@@ -1,78 +1,59 @@
 import { useModeViewOrder } from "@/hooks/use-mode-view-order";
 import { useStore } from "@/hooks/use-store";
-import { motion } from "framer-motion";
-import {
-  BetweenVerticalStart,
-  LucideProps,
-  TableProperties,
-} from "lucide-react";
-import { ForwardRefExoticComponent, RefAttributes } from "react";
+import { BetweenVerticalStart, TableProperties } from "lucide-react";
+import { useEffect, useState } from "react";
 
 import { Button } from "../ui/button";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "../ui/tooltip";
+import { Tooltip, TooltipContent, TooltipTrigger } from "../ui/tooltip";
 
-interface ViewData {
-  id: "table" | "kanban";
-  icon: ForwardRefExoticComponent<
-    Omit<LucideProps, "ref"> & RefAttributes<SVGSVGElement>
-  >;
-  tooltip: string;
-}
-
-const viewData: ViewData[] = [
-  {
-    id: "table",
-    icon: TableProperties,
-    tooltip: "Ver pedidos en formato de tabla",
-  },
-  {
-    id: "kanban",
-    icon: BetweenVerticalStart,
-    tooltip: "Ver pedidos en formato de kanban",
-  },
-];
+export type ViewType = "table" | "kanban";
 
 export const SwitchModeView = () => {
-  const modeView = useStore(useModeViewOrder, (state) => state);
+  const mode = useStore(useModeViewOrder, (state) => state);
 
-  if (!modeView) null;
+  const [activeView, setActiveView] = useState<ViewType>(
+    mode?.modeViewOrder || "table",
+  );
+
+  useEffect(() => {
+    mode?.setModeViewOrder(activeView);
+  }, [activeView]);
+
   return (
     <div className="flex justify-end">
-      <div className="flex w-fit space-x-1 rounded-xl bg-secondary p-1">
-        {viewData.map(({ id, icon: Icon, tooltip }) => (
-          <Button
-            key={id}
-            variant="ghost"
-            className={`${
-              modeView?.modeViewOrder === id
-                ? "text-slate-500"
-                : "text-muted-foreground hover:text-primary"
-            } relative z-10 flex items-center justify-center rounded-lg px-[10px] text-sm font-medium transition focus-visible:outline-2`}
-            onClick={() => modeView?.setModeViewOrder(id)}
-          >
-            <TooltipProvider>
-              <Tooltip>
-                <TooltipTrigger asChild>
-                  <Icon className="size-5" strokeWidth={1.5} />
-                </TooltipTrigger>
-                <TooltipContent>{tooltip}</TooltipContent>
-              </Tooltip>
-            </TooltipProvider>
-            {modeView?.modeViewOrder === id && (
-              <motion.span
-                layoutId="bubble"
-                className="absolute inset-0 -z-10 bg-white"
-                style={{ borderRadius: 8 }}
-                transition={{ type: "spring", bounce: 0.2, duration: 0.6 }}
-              />
-            )}
-          </Button>
-        ))}
+      <div className="flex h-fit w-fit space-x-1 rounded-md p-1">
+        <Button
+          size="icon"
+          variant="outline"
+          className={`${
+            activeView === "table" ? "bg-white hover:bg-white" : "bg-accent"
+          } relative z-10 flex items-center justify-center text-sm font-medium transition focus-visible:outline-2`}
+          onClick={() => setActiveView && setActiveView("table")}
+        >
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <TableProperties size={20} />
+            </TooltipTrigger>
+            <TooltipContent>Modo tabla</TooltipContent>
+          </Tooltip>
+        </Button>
+        <Button
+          size="icon"
+          variant="outline"
+          className={`${
+            activeView === "kanban" ? "bg-white hover:bg-white" : "bg-accent"
+          } relative z-10 flex items-center justify-center text-sm font-medium transition focus-visible:outline-2`}
+          onClick={() => setActiveView && setActiveView("kanban")}
+        >
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <BetweenVerticalStart size={20} />
+            </TooltipTrigger>
+            <TooltipContent>Modo tabla</TooltipContent>
+          </Tooltip>
+        </Button>
+        {/* {viewData.map(({ id, icon: Icon, tooltip }) => (
+        ))} */}
       </div>
     </div>
   );
