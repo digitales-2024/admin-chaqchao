ec4bae0e359153fc01a7a85ecaa06fd107192dd8
EleoRomario <eleo.romario@gmail.com>
2024-10-05 10:41:17 -0500

    feat: crear el manejo de detalles de pedido en TableOrders.tsx

--- src/components/orders/OrderSheetDetails.tsx
+++ src/components/orders/OrderSheetDetails.tsx
@@ -1,4 +1,17 @@
-import { Copy, CreditCard, MoreVertical, Truck } from "lucide-react";
+import { useOrders } from "@/hooks/use-orders";
+import { Order } from "@/types";
+import { format } from "date-fns";
+import { es } from "date-fns/locale";
+import {
+  Apple,
+  Copy,
+  CreditCard,
+  Mail,
+  MessageCircleMore,
+  MoreVertical,
+  PackageX,
+  Phone,
+} from "lucide-react";
 
 import { Button } from "@/components/ui/button";
 import { CardDescription, CardTitle } from "@/components/ui/card";
@@ -7,160 +20,228 @@ import {
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuSeparator,
+  DropdownMenuShortcut,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Separator } from "@/components/ui/separator";
 
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import { Badge } from "../ui/badge";
 import { ScrollArea } from "../ui/scroll-area";
 import { Sheet, SheetContent, SheetFooter, SheetTitle } from "../ui/sheet";
+import { iconsStatus, statusColors, translateStatus } from "./kanban/OrderCard";
 
 interface OrderSheetDetailsProps {
+  order: Order | null;
   open: boolean;
   onOpenChange: () => void;
 }
 
 export const OrderSheetDetails = ({
+  order,
   open,
   onOpenChange,
 }: OrderSheetDetailsProps) => {
+  const { orderById } = useOrders({
+    id: order?.id,
+  });
   return (
     <Sheet open={open} onOpenChange={onOpenChange}>
-      <ScrollArea className="h-screen">
-        <SheetContent className="flex flex-col gap-5 lg:max-w-[36rem]">
-          <SheetTitle className="flex flex-row items-start bg-muted/50 py-4">
-            <div className="grid gap-0.5">
-              <CardTitle className="group flex items-center gap-2 text-lg">
-                Order Oe31b70H
-                <Button
-                  size="icon"
-                  variant="outline"
-                  className="h-6 w-6 opacity-0 transition-opacity group-hover:opacity-100"
-                >
-                  <Copy className="h-3 w-3" />
-                  <span className="sr-only">Copy Order ID</span>
-                </Button>
-              </CardTitle>
-              <CardDescription>Date: November 23, 2023</CardDescription>
-            </div>
-            <div className="ml-auto flex items-center gap-1">
-              <Button size="sm" variant="outline" className="h-8 gap-1">
-                <Truck className="h-3.5 w-3.5" />
-                <span className="lg:sr-only xl:not-sr-only xl:whitespace-nowrap">
-                  Track Order
-                </span>
+      <SheetContent className="flex h-full min-h-screen flex-col gap-5 lg:max-w-[36rem]">
+        <SheetTitle className="flex flex-row items-start bg-muted/50 py-4">
+          <div className="grid gap-0.5">
+            <CardTitle className="group flex items-center gap-2 text-lg">
+              <span className="font-thin uppercase text-slate-500">
+                Pedido#{" "}
+              </span>
+              {orderById?.pickupCode}
+              <Button
+                size="icon"
+                variant="outline"
+                className="h-6 w-6 opacity-0 transition-opacity group-hover:opacity-100"
+              >
+                <Copy className="h-3 w-3" />
+                <span className="sr-only">Copy Order ID</span>
               </Button>
-              <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                  <Button size="icon" variant="outline" className="h-8 w-8">
-                    <MoreVertical className="h-3.5 w-3.5" />
-                    <span className="sr-only">More</span>
-                  </Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent align="end">
-                  <DropdownMenuItem>Edit</DropdownMenuItem>
-                  <DropdownMenuItem>Export</DropdownMenuItem>
-                  <DropdownMenuSeparator />
-                  <DropdownMenuItem>Trash</DropdownMenuItem>
-                </DropdownMenuContent>
-              </DropdownMenu>
-            </div>
-          </SheetTitle>
-          <div className="grid gap-4">
-            <div className="font-semibold">Order Details</div>
-            <ul className="grid gap-3">
-              <li className="flex items-center justify-between">
-                <span className="text-muted-foreground">
-                  Glimmer Lamps x <span>2</span>
-                </span>
-                <span>$250.00</span>
-              </li>
-              <li className="flex items-center justify-between">
-                <span className="text-muted-foreground">
-                  Aqua Filters x <span>1</span>
-                </span>
-                <span>$49.00</span>
-              </li>
-            </ul>
-            <Separator className="my-2" />
-            <ul className="grid gap-3">
-              <li className="flex items-center justify-between">
-                <span className="text-muted-foreground">Subtotal</span>
-                <span>$299.00</span>
-              </li>
-              <li className="flex items-center justify-between">
-                <span className="text-muted-foreground">Shipping</span>
-                <span>$5.00</span>
-              </li>
-              <li className="flex items-center justify-between">
-                <span className="text-muted-foreground">Tax</span>
-                <span>$25.00</span>
-              </li>
-              <li className="flex items-center justify-between font-semibold">
-                <span className="text-muted-foreground">Total</span>
-                <span>$329.00</span>
-              </li>
-            </ul>
+            </CardTitle>
+            <CardDescription className="text-xs font-thin">
+              Fecha:{" "}
+              <span>
+                {format(
+                  orderById ? orderById?.pickupTime : new Date(),
+                  "PPPp",
+                  {
+                    locale: es,
+                  },
+                )}
+              </span>
+            </CardDescription>
           </div>
-          <Separator className="my-4" />
-          <div className="grid grid-cols-2 gap-4">
+          <div className="ml-auto flex items-center gap-1">
+            <Badge
+              variant="outline"
+              className={`${statusColors[order?.orderStatus as keyof typeof statusColors]} flex gap-2 font-light`}
+            >
+              {iconsStatus[order?.orderStatus as keyof typeof iconsStatus]}
+              {
+                translateStatus[
+                  order?.orderStatus as keyof typeof translateStatus
+                ]
+              }
+            </Badge>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button size="icon" variant="outline" className="h-8 w-8">
+                  <MoreVertical className="h-3.5 w-3.5" />
+                  <span className="sr-only">Mas</span>
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end">
+                <DropdownMenuItem>Exportar</DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>
+                  Cancelar
+                  <DropdownMenuShortcut>
+                    <PackageX className="size-4" aria-hidden="true" />
+                  </DropdownMenuShortcut>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+        </SheetTitle>
+        <div className="flex-1 px-6 py-2">
+          <ScrollArea>
+            <div className="grid gap-4">
+              <div className="font-semibold">Detalles del pedido</div>
+              <ul className="grid gap-3">
+                {orderById?.cart.map((item) => (
+                  <li
+                    className="flex items-center justify-between"
+                    key={item.id}
+                  >
+                    <div className="flex items-center gap-2 truncate">
+                      <Avatar className="bg-slate-100">
+                        <AvatarImage src={item.image} alt={item.name} />
+                        <AvatarFallback>
+                          <Apple />
+                        </AvatarFallback>
+                      </Avatar>
+                      <span className="text-muted-foreground">
+                        {item.name} x{" "}
+                        <span className="text-xs">{item.quantity}</span>
+                      </span>
+                    </div>
+                    <span>S/.{item.price}</span>
+                  </li>
+                ))}
+              </ul>
+              <Separator className="my-2" />
+              <ul className="grid gap-3">
+                <li className="flex items-center justify-between">
+                  <span className="text-xs font-extralight text-slate-400">
+                    Subtotal
+                  </span>
+                  <span>
+                    S/.
+                    {orderById?.cart.reduce(
+                      (acc, item) => acc + item.price * item.quantity,
+                      0,
+                    )}
+                  </span>
+                </li>
+                <li className="flex items-center justify-between">
+                  <span className="text-xs font-extralight text-slate-400">
+                    Impuesto
+                  </span>
+                  <span>S/.00</span>
+                </li>
+                <li className="flex items-center justify-between font-semibold">
+                  <span className="text-muted-foreground">Total</span>
+                  <span>S/. 000</span>
+                </li>
+              </ul>
+            </div>
+            <Separator className="my-4" />
             <div className="grid gap-3">
-              <div className="font-semibold">Shipping Information</div>
-              <address className="grid gap-0.5 not-italic text-muted-foreground">
-                <span>Liam Johnson</span>
-                <span>1234 Main St.</span>
-                <span>Anytown, CA 12345</span>
-              </address>
+              <div className="font-semibold">Información del cliente</div>
+              <dl className="grid gap-3">
+                <div className="flex items-center justify-between">
+                  <dt className="text-sm text-muted-foreground">Cliente</dt>
+                  <dd className="capitalize">{orderById?.client.name}</dd>
+                </div>
+                <div className="flex items-center justify-between">
+                  <dt className="text-sm text-muted-foreground">
+                    Correo electrónico
+                  </dt>
+                  <dd className="group/email">
+                    <Button
+                      size="icon"
+                      variant="outline"
+                      className="h-6 w-6 opacity-0 transition-opacity group-hover/email:opacity-100"
+                    >
+                      <span className="sr-only">Email client</span>
+                      <a href="mailto:">
+                        <Mail className="h-3 w-3" />
+                      </a>
+                    </Button>
+                    {orderById?.client.email}
+                  </dd>
+                </div>
+                <div className="flex items-center justify-between">
+                  <dt className="text-sm text-muted-foreground">Teléfono</dt>
+                  <dd className="group/phone space-x-2">
+                    <Button
+                      size="icon"
+                      variant="outline"
+                      className="h-6 w-6 opacity-0 transition-opacity group-hover/phone:opacity-100"
+                    >
+                      <span className="sr-only">Phone client</span>
+                      <a href="tel:">
+                        <Phone className="h-3 w-3" />
+                      </a>
+                    </Button>
+                    <Button
+                      size="icon"
+                      variant="outline"
+                      className="h-6 w-6 opacity-0 transition-opacity group-hover/phone:opacity-100"
+                    >
+                      <span className="sr-only">Phone client</span>
+                      <a
+                        href={`https://wa.me/${orderById?.client.phone.replace(/\s+/g, "")}`}
+                        target="_blank"
+                      >
+                        <MessageCircleMore className="h-3 w-3" />
+                      </a>
+                    </Button>
+                    {orderById?.client.phone}
+                  </dd>
+                </div>
+              </dl>
             </div>
-            <div className="grid auto-rows-max gap-3">
-              <div className="font-semibold">Billing Information</div>
-              <div className="text-muted-foreground">
-                Same as shipping address
-              </div>
+            <Separator className="my-4" />
+            <div className="grid gap-3">
+              <div className="font-semibold">Información de pago</div>
+              <dl className="grid gap-3">
+                <div className="flex items-center justify-between">
+                  <dt className="flex items-center gap-1 text-muted-foreground">
+                    <CreditCard className="h-4 w-4" />
+                    Tarjeta de crédito
+                  </dt>
+                  <dd>**** **** **** 4532</dd>
+                </div>
+              </dl>
             </div>
-          </div>
-          <Separator className="my-4" />
-          <div className="grid gap-3">
-            <div className="font-semibold">Customer Information</div>
-            <dl className="grid gap-3">
-              <div className="flex items-center justify-between">
-                <dt className="text-muted-foreground">Customer</dt>
-                <dd>Liam Johnson</dd>
-              </div>
-              <div className="flex items-center justify-between">
-                <dt className="text-muted-foreground">Email</dt>
-                <dd>
-                  <a href="mailto:">liam@acme.com</a>
-                </dd>
-              </div>
-              <div className="flex items-center justify-between">
-                <dt className="text-muted-foreground">Phone</dt>
-                <dd>
-                  <a href="tel:">+1 234 567 890</a>
-                </dd>
-              </div>
-            </dl>
-          </div>
-          <Separator className="my-4" />
-          <div className="grid gap-3">
-            <div className="font-semibold">Payment Information</div>
-            <dl className="grid gap-3">
-              <div className="flex items-center justify-between">
-                <dt className="flex items-center gap-1 text-muted-foreground">
-                  <CreditCard className="h-4 w-4" />
-                  Visa
-                </dt>
-                <dd>**** **** **** 4532</dd>
-              </div>
-            </dl>
-          </div>
 
-          <SheetFooter className="flex flex-row items-center border-t bg-muted/50 px-6 py-3">
-            <div className="text-xs text-muted-foreground">
-              Updated <time dateTime="2023-11-23">November 23, 2023</time>
-            </div>
-          </SheetFooter>
-        </SheetContent>
-      </ScrollArea>
+            <SheetFooter className="flex flex-row items-center border-t bg-muted/50 px-6 py-3">
+              <div className="text-xs text-muted-foreground">
+                {orderById?.updatedAt
+                  ? format(orderById.updatedAt, "PPPp", { locale: es })
+                  : "Fecha no disponible"}
+              </div>
+            </SheetFooter>
+          </ScrollArea>
+        </div>
+      </SheetContent>
     </Sheet>
   );
 };
