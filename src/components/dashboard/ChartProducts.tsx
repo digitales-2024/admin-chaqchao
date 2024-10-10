c3ab56a7c220a82a8831b012a6e9148a08e99427
EleoRomario <eleo.romario@gmail.com>
2024-10-10 11:57:43 -0500

    feat: agregar funcionalidad de mostrar los 5 productos más vendidos en un gráfico radial

--- src/components/dashboard/ChartProducts.tsx
+++ src/components/dashboard/ChartProducts.tsx
@@ -1,10 +1,12 @@
 "use client";
-import { TrendingUp } from "lucide-react";
-import { LabelList, RadialBar, RadialBarChart } from "recharts";
+import { useData } from "@/hooks/use-data";
+import { getStartEndDate } from "@/utils/getStartEndDate";
+import { PolarGrid, RadialBar, RadialBarChart } from "recharts";
 
 import {
   Card,
   CardContent,
+  CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
@@ -16,78 +18,91 @@ import {
   ChartTooltipContent,
 } from "../ui/chart";
 
-const chartData = [
-  { browser: "chrome", visitors: 275, fill: "var(--color-chrome)" },
-  { browser: "safari", visitors: 200, fill: "var(--color-safari)" },
-  { browser: "firefox", visitors: 187, fill: "var(--color-firefox)" },
-  { browser: "edge", visitors: 173, fill: "var(--color-edge)" },
-  { browser: "other", visitors: 90, fill: "var(--color-other)" },
-];
 const chartConfig = {
-  visitors: {
-    label: "Visitors",
-  },
-  chrome: {
-    label: "Chrome",
-    color: "hsl(var(--chart-1))",
-  },
-  safari: {
-    label: "Safari",
-    color: "hsl(var(--chart-2))",
-  },
-  firefox: {
-    label: "Firefox",
-    color: "hsl(var(--chart-3))",
-  },
-  edge: {
-    label: "Edge",
-    color: "hsl(var(--chart-4))",
-  },
-  other: {
-    label: "Other",
-    color: "hsl(var(--chart-5))",
+  totalOrdered: {
+    label: "Total",
   },
 } satisfies ChartConfig;
 
 export const ChartProducts = () => {
+  const { start, end } = getStartEndDate();
+
+  const { topProducts } = useData({
+    startDate: start,
+    endDate: end,
+  });
+
+  const generateColors = (count: number) => {
+    return Array.from({ length: count }, (_, i) => {
+      const opacity = 1 - i * 0.15;
+      return `hsl(var(--primary) / ${opacity})`;
+    });
+  };
+
+  const colors = generateColors(topProducts?.length ?? 0);
+
+  // Solo obtener los 5 productos más vendidos
+  const chartData = topProducts?.slice(0, 5).map((producto, index) => ({
+    ...producto,
+    fill: colors[index % colors.length],
+  }));
+
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle>Productos Más Vendidos</CardTitle>
+    <Card className="w-full max-w-4xl">
+      <CardHeader className="pb-2">
+        <CardTitle>Top Productos Más Vendidos</CardTitle>
+        <CardDescription>Últimos 30 días</CardDescription>
       </CardHeader>
-      <CardContent className="flex-1 pb-0">
-        <ChartContainer
-          config={chartConfig}
-          className="mx-auto aspect-square max-h-[250px]"
-        >
-          <RadialBarChart
-            data={chartData}
-            startAngle={-90}
-            endAngle={380}
-            innerRadius={30}
-            outerRadius={110}
+      <CardContent className="pb-0">
+        <div className="flex flex-col items-start justify-between gap-4 md:flex-row">
+          <ChartContainer
+            config={chartConfig}
+            className="aspect-square h-[300px] w-full md:w-2/3"
           >
-            <ChartTooltip
-              cursor={false}
-              content={<ChartTooltipContent hideLabel nameKey="browser" />}
-            />
-            <RadialBar dataKey="visitors" background>
-              <LabelList
-                position="insideStart"
-                dataKey="browser"
-                className="fill-white capitalize mix-blend-luminosity"
-                fontSize={11}
+            <RadialBarChart
+              data={chartData}
+              innerRadius="30%"
+              outerRadius="90%"
+              startAngle={180}
+              endAngle={0}
+            >
+              <ChartTooltip
+                cursor={false}
+                content={<ChartTooltipContent hideLabel nameKey="name" />}
               />
-            </RadialBar>
-          </RadialBarChart>
-        </ChartContainer>
-      </CardContent>
-      <CardFooter className="flex-col gap-2 text-sm">
-        <div className="flex items-center gap-2 font-medium leading-none">
-          Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
+              <PolarGrid gridType="circle" />
+              <RadialBar
+                dataKey="totalOrdered"
+                label={{
+                  position: "insideRight",
+                  fill: "#fff",
+                  fontWeight: "bold",
+                  fontSize: 12,
+                  offset: 10,
+                }}
+              />
+            </RadialBarChart>
+          </ChartContainer>
+          <div className="flex w-full flex-col justify-center md:w-1/3">
+            <h3 className="mb-2 text-lg font-semibold">Leyenda</h3>
+            <ul className="space-y-2">
+              {chartData?.map((producto) => (
+                <li key={producto.id} className="flex items-center gap-2">
+                  <div
+                    className="h-3 w-3 flex-shrink-0 rounded-full"
+                    style={{ backgroundColor: producto.fill }}
+                    aria-hidden="true"
+                  />
+                  <span className="text-sm">{producto.name}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
+      </CardContent>
+      <CardFooter className="mt-4 flex-col gap-2 text-sm">
         <div className="leading-none text-muted-foreground">
-          Showing total visitors for the last 6 months
+          Mostrando los 5 productos más vendidos en los últimos 30 días
         </div>
       </CardFooter>
     </Card>
