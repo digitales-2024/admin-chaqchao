f61caa5fa1b308d490051498d6918ba38d882943
EleoRomario <eleo.romario@gmail.com>
2024-10-10 17:07:39 -0500

    refactor: actualizar diseño de la página de inicio

--- src/components/dashboard/Greeting.tsx
+++ src/components/dashboard/Greeting.tsx
@@ -6,7 +6,8 @@ export const Greeting = () => {
   const { user } = useProfile();
   return (
     <h1 className="inline-flex flex-wrap gap-2 text-2xl font-semibold text-gray-800">
-      {getGreeting()},<span className="font-medium">{user?.name}</span>
+      {getGreeting()},
+      <span className="font-medium capitalize">{user?.name}</span>
     </h1>
   );
 };
