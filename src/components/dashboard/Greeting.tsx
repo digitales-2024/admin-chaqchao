7fee07ffb76acd79a047f52d4728584ace0f2b40
EleoRomario <eleo.romario@gmail.com>
2024-10-10 16:15:29 -0500

    feat: actualizar estilo de componente Greeting para permitir el ajuste automático del texto

--- src/components/dashboard/Greeting.tsx
+++ src/components/dashboard/Greeting.tsx
@@ -5,7 +5,7 @@ import { getGreeting } from "@/utils/getGreeting";
 export const Greeting = () => {
   const { user } = useProfile();
   return (
-    <h1 className="inline-flex gap-2 text-2xl font-semibold text-gray-800">
+    <h1 className="inline-flex flex-wrap gap-2 text-2xl font-semibold text-gray-800">
       {getGreeting()},<span className="font-medium">{user?.name}</span>
     </h1>
   );
