d55f6049102c22d659119c2ecf2a2b3a100964e6
Diego Rondón <furrtr4532@gmail.com>
2024-10-10 17:01:39 -0500

    feat: agregar mensajes a las funciones de exportación a Excel y PDF en el hook useReports

--- src/hooks/use-reports.ts
+++ src/hooks/use-reports.ts
@@ -5,62 +5,39 @@ import {
   useExportProductsToExcelMutation,
   useExportTopProductsToPdfMutation,
   useExportTopProductsToExcelMutation,
+  useGetProductsReportQuery,
+  useGetOrdersReportQuery,
 } from "@/redux/services/reportsApi";
 import { FilterOrdersSchema } from "@/schemas/reports/filterOrdersSchema";
 import { FilterProductSchema } from "@/schemas/reports/filterProductSchema";
 import { toast } from "sonner";
 
-// Hook para exportar reportes
+// Hook para exportar y obtener reportes
 export const useReports = () => {
-  const [
-    exportOrdersToPdf,
-    { isLoading: isLoadingExportOrdersPdf, error: errorExportOrdersPdf },
-  ] = useExportOrdersToPdfMutation();
+  // Mutaciones para exportar órdenes y productos
+  const [exportOrdersToPdf] = useExportOrdersToPdfMutation();
+  const [exportOrdersToExcel] = useExportOrdersToExcelMutation();
+  const [exportProductsToPdf] = useExportProductsToPdfMutation();
+  const [exportProductsToExcel] = useExportProductsToExcelMutation();
+  const [exportTopProductsToPdf] = useExportTopProductsToPdfMutation();
+  const [exportTopProductsToExcel] = useExportTopProductsToExcelMutation();
 
-  const [
-    exportOrdersToExcel,
-    { isLoading: isLoadingExportOrdersExcel, error: errorExportOrdersExcel },
-  ] = useExportOrdersToExcelMutation();
+  // Obtener reportes de productos
+  const useGetProductsReport = (filter: FilterProductSchema) => {
+    return useGetProductsReportQuery({ filter });
+  };
 
-  const [
-    exportProductsToPdf,
-    { isLoading: isLoadingExportProductsPdf, error: errorExportProductsPdf },
-  ] = useExportProductsToPdfMutation();
+  // Obtener reportes de órdenes
+  const useGetOrdersReport = (filter: FilterOrdersSchema) => {
+    return useGetOrdersReportQuery({ filter });
+  };
 
-  const [
-    exportProductsToExcel,
-    {
-      isLoading: isLoadingExportProductsExcel,
-      error: errorExportProductsExcel,
-    },
-  ] = useExportProductsToExcelMutation();
-
-  const [
-    exportTopProductsToPdf,
-    {
-      isLoading: isLoadingExportTopProductsPdf,
-      error: errorExportTopProductsPdf,
-    },
-  ] = useExportTopProductsToPdfMutation();
-
-  const [
-    exportTopProductsToExcel,
-    {
-      isLoading: isLoadingExportTopProductsExcel,
-      error: errorExportTopProductsExcel,
-    },
-  ] = useExportTopProductsToExcelMutation();
-
-  /**
-   * Descargar las órdenes en un archivo PDF
-   * @param filter Filtros de órdenes
-   */
+  // Función para exportar las órdenes a PDF
   const exportOrdersReportToPdf = async (filter: FilterOrdersSchema) => {
     const promise = () =>
       new Promise(async (resolve, reject) => {
         try {
           const response = await exportOrdersToPdf({ filter }).unwrap();
-
           const url = window.URL.createObjectURL(response);
           const link = document.createElement("a");
           link.href = url;
@@ -69,32 +46,26 @@ export const useReports = () => {
           link.click();
           link.remove();
           window.URL.revokeObjectURL(url);
-
-          resolve("Pedidos exportadas a PDF con éxito");
+          resolve("Órdenes exportadas a PDF con éxito");
         } catch (error) {
-          console.error("Error exporting orders to PDF:", error);
+          console.error("Error al exportar órdenes a PDF:", error);
           reject(new Error("Error al exportar órdenes a PDF"));
         }
       });
 
     return toast.promise(promise(), {
-      loading: "Exportando pedidos a PDF...",
-      success: "Pedidos exportadas a PDF con éxito",
+      loading: "Exportando órdenes a PDF...",
+      success: "Órdenes exportadas a PDF con éxito",
       error: (err) => err.message,
     });
   };
 
-  /**
-   * Descargar las órdenes en un archivo Excel
-   * @param filter Filtros de órdenes
-   * @returns Excel Blob
-   */
+  // Función para exportar las órdenes a Excel
   const exportOrdersReportToExcel = async (filter: FilterOrdersSchema) => {
     const promise = () =>
       new Promise(async (resolve, reject) => {
         try {
           const response = await exportOrdersToExcel({ filter }).unwrap();
-
           const url = window.URL.createObjectURL(new Blob([response]));
           const link = document.createElement("a");
           link.href = url;
@@ -102,31 +73,26 @@ export const useReports = () => {
           document.body.appendChild(link);
           link.click();
           link.remove();
-
-          resolve("Pedidos exportadas a Excel con éxito");
+          resolve("Órdenes exportadas a Excel con éxito");
         } catch (error) {
-          console.error("Error exporting orders to Excel:", error);
+          console.error("Error al exportar órdenes a Excel:", error);
           reject(new Error("Error al exportar órdenes a Excel"));
         }
       });
 
     return toast.promise(promise(), {
-      loading: "Exportando pedidos a Excel...",
-      success: "Pedidos exportadas a Excel con éxito",
+      loading: "Exportando órdenes a Excel...",
+      success: "Órdenes exportadas a Excel con éxito",
       error: (err) => err.message,
     });
   };
 
-  /**
-   * Descargar los productos en un archivo PDF
-   * @param filter Filtros de productos
-   */
+  // Función para exportar los productos a PDF
   const exportProductsReportToPdf = async (filter: FilterProductSchema) => {
     const promise = () =>
       new Promise(async (resolve, reject) => {
         try {
           const response = await exportProductsToPdf({ filter }).unwrap();
-
           const url = window.URL.createObjectURL(response);
           const link = document.createElement("a");
           link.href = url;
@@ -135,10 +101,9 @@ export const useReports = () => {
           link.click();
           link.remove();
           window.URL.revokeObjectURL(url);
-
           resolve("Productos exportados a PDF con éxito");
         } catch (error) {
-          console.error("Error exporting products to PDF:", error);
+          console.error("Error al exportar productos a PDF:", error);
           reject(new Error("Error al exportar productos a PDF"));
         }
       });
@@ -150,17 +115,12 @@ export const useReports = () => {
     });
   };
 
-  /**
-   * Descargar los productos en un archivo Excel
-   * @param filter Filtros de productos
-   * @returns Excel Blob
-   */
+  // Función para exportar los productos a Excel
   const exportProductsReportToExcel = async (filter: FilterProductSchema) => {
     const promise = () =>
       new Promise(async (resolve, reject) => {
         try {
           const response = await exportProductsToExcel({ filter }).unwrap();
-
           const url = window.URL.createObjectURL(new Blob([response]));
           const link = document.createElement("a");
           link.href = url;
@@ -168,10 +128,9 @@ export const useReports = () => {
           document.body.appendChild(link);
           link.click();
           link.remove();
-
           resolve("Productos exportados a Excel con éxito");
         } catch (error) {
-          console.error("Error exporting products to Excel:", error);
+          console.error("Error al exportar productos a Excel:", error);
           reject(new Error("Error al exportar productos a Excel"));
         }
       });
@@ -183,16 +142,12 @@ export const useReports = () => {
     });
   };
 
-  /**
-   * Descargar los productos top en un archivo PDF
-   * @param filter Filtros de productos top
-   */
+  // Función para exportar productos top a PDF
   const exportTopProductsReportToPdf = async (filter: FilterProductSchema) => {
     const promise = () =>
       new Promise(async (resolve, reject) => {
         try {
           const response = await exportTopProductsToPdf({ filter }).unwrap();
-
           const url = window.URL.createObjectURL(response);
           const link = document.createElement("a");
           link.href = url;
@@ -201,10 +156,9 @@ export const useReports = () => {
           link.click();
           link.remove();
           window.URL.revokeObjectURL(url);
-
           resolve("Productos top exportados a PDF con éxito");
         } catch (error) {
-          console.error("Error exporting top products to PDF:", error);
+          console.error("Error al exportar productos top a PDF:", error);
           reject(new Error("Error al exportar productos top a PDF"));
         }
       });
@@ -216,11 +170,7 @@ export const useReports = () => {
     });
   };
 
-  /**
-   * Descargar los productos top en un archivo Excel
-   * @param filter Filtros de productos top
-   * @returns Excel Blob
-   */
+  // Función para exportar productos top a Excel
   const exportTopProductsReportToExcel = async (
     filter: FilterProductSchema,
   ) => {
@@ -228,7 +178,6 @@ export const useReports = () => {
       new Promise(async (resolve, reject) => {
         try {
           const response = await exportTopProductsToExcel({ filter }).unwrap();
-
           const url = window.URL.createObjectURL(new Blob([response]));
           const link = document.createElement("a");
           link.href = url;
@@ -236,10 +185,9 @@ export const useReports = () => {
           document.body.appendChild(link);
           link.click();
           link.remove();
-
           resolve("Productos top exportados a Excel con éxito");
         } catch (error) {
-          console.error("Error exporting top products to Excel:", error);
+          console.error("Error al exportar productos top a Excel:", error);
           reject(new Error("Error al exportar productos top a Excel"));
         }
       });
@@ -251,24 +199,15 @@ export const useReports = () => {
     });
   };
 
+  // Retornamos todas las funciones y estados necesarios
   return {
     exportOrdersReportToPdf,
-    isLoadingExportOrdersPdf,
-    errorExportOrdersPdf,
     exportOrdersReportToExcel,
-    isLoadingExportOrdersExcel,
-    errorExportOrdersExcel,
     exportProductsReportToPdf,
-    isLoadingExportProductsPdf,
-    errorExportProductsPdf,
     exportProductsReportToExcel,
-    isLoadingExportProductsExcel,
-    errorExportProductsExcel,
     exportTopProductsReportToPdf,
-    isLoadingExportTopProductsPdf,
-    errorExportTopProductsPdf,
     exportTopProductsReportToExcel,
-    isLoadingExportTopProductsExcel,
-    errorExportTopProductsExcel,
+    useGetOrdersReport,
+    useGetProductsReport,
   };
 };
