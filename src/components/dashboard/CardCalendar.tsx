c0dcc6fef19925d2fce6de97a73b3ed1a746d3e2
EleoRomario <eleo.romario@gmail.com>
2024-10-10 16:15:10 -0500

    feat: actualizar componente CardCalendar para mostrar la fecha actual en formato legible

--- src/components/dashboard/CardCalendar.tsx
+++ src/components/dashboard/CardCalendar.tsx
@@ -6,14 +6,20 @@ import { Card } from "../ui/card";
 
 export const CardCalendar = () => {
   return (
-    <Card className="flex flex-col gap-4 py-10">
-      <p className="text-center font-medium">
-        Hoy{" "}
-        {new Date().toLocaleDateString("es-ES", {
-          weekday: "long",
-          day: "numeric",
-          month: "long",
-        })}
+    <Card className="flex flex-col gap-4 p-10">
+      <p className="flex flex-col text-right font-medium">
+        <span>Hoy</span>
+        <span className="text-4xl font-bold uppercase">
+          {new Date().toLocaleDateString("es-ES", {
+            weekday: "long",
+          })}
+        </span>
+        <span>
+          {new Date().toLocaleDateString("es-ES", {
+            day: "numeric",
+            month: "long",
+          })}
+        </span>
       </p>
       <Calendar className="flex justify-center" locale={es} />
     </Card>
