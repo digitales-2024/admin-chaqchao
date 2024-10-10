46068f9a20030802d3535022fbcd05f651025785
FrancoGT <francogt8000@gmail.com>
2024-10-10 16:48:48 -0500

    feat: mejoras a los estilos de detalles de pedido

--- src/components/clients/ClientOrderDetails.tsx
+++ src/components/clients/ClientOrderDetails.tsx
@@ -2,12 +2,13 @@ import { useOrders } from "@/hooks/use-orders";
 import { format } from "date-fns";
 import { es } from "date-fns/locale";
 
+import { DataTableSkeleton } from "../data-table/DataTableSkeleton";
 import { translateStatus } from "../orders/OrderSheetDetails";
 import { Badge } from "../ui/badge";
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
 
 interface ClientOrderDetailsProps {
   orderId: string;
-  comments?: string; //Recupero los comentarios
 }
 
 export function ClientOrderDetails({ orderId }: ClientOrderDetailsProps) {
@@ -16,14 +17,23 @@ export function ClientOrderDetails({ orderId }: ClientOrderDetailsProps) {
   });
 
   if (!orderById) {
-    // Si el pedido no ha sido cargado o no existe
-    return <p>Cargando detalles del pedido...</p>;
+    // Si el pedido no ha sido cargado o no existe, muestra skeleton
+    return (
+      <DataTableSkeleton
+        columnCount={5}
+        rowCount={4}
+        searchableColumnCount={2}
+        filterableColumnCount={1}
+        showViewOptions={true}
+        cellWidths={["20%", "20%", "20%", "20%", "20%"]}
+        withPagination={true}
+      />
+    );
   }
+
   let badgeClass = "bg-gray-100 text-gray-500";
-  //para poder traducir y usar estilos a la vez en los estados
   const statusText = translateStatus[orderById?.orderStatus] || "Desconocido";
 
-  // Aquí hacemos el switch sobre el estado del pedido
   switch (orderById?.orderStatus) {
     case "PENDING":
       badgeClass = "bg-yellow-100 text-yellow-500";
@@ -44,50 +54,64 @@ export function ClientOrderDetails({ orderId }: ClientOrderDetailsProps) {
       badgeClass = "bg-gray-100 text-gray-500";
       break;
   }
-  return (
-    <div className="rounded-md bg-gray-50 p-4">
-      <h4 className="mb-2 font-semibold">
-        Detalles del pedido: {orderById?.pickupCode || "--"}
-      </h4>
-      <p className="mb-2 text-sm">
-        Estado:{" "}
-        <div className="cursor-pointer">
-          <Badge variant="secondary" className={badgeClass}>
-            {statusText}
-          </Badge>
-        </div>
-      </p>
-      <p className="mb-2 text-sm">
-        Fecha de recogida:{" "}
-        {orderById?.pickupTime
-          ? format(new Date(orderById.pickupTime), "PPPp", {
-              locale: es,
-            })
-          : "--"}
-      </p>
-      <p className="mb-2 text-sm">
-        Dirección de recogida: {orderById?.pickupAddress || "--"}
-      </p>
 
-      <h4 className="mb-2 font-semibold">Productos:</h4>
-      {orderById?.cart?.products?.length > 0 ? (
-        <ul className="mb-4">
-          {orderById.cart.products.map((product) => (
-            <li key={product.id} className="mb-2 flex">
-              <div>
-                <p className="font-semibold">{product.name}</p>
-                <p className="text-sm">Categoría: {product.category.name}</p>
-                <p className="text-sm">
-                  Precio: S/ {product.price.toFixed(2)} x {product.quantity}{" "}
-                  unidades
-                </p>
+  return (
+    <Card className="max-h-[500px] w-full overflow-y-auto rounded-lg shadow-lg">
+      <CardHeader className="rounded-t-lg bg-gradient-to-r">
+        <CardTitle className="text-xl font-bold">Detalles del pedido</CardTitle>
+      </CardHeader>
+      <CardContent className="bg-white p-4">
+        <div className="mb-4 grid grid-cols-2 gap-4">
+          <div>
+            <p className="text-sm font-semibold">Estado:</p>
+            <Badge variant="secondary" className={badgeClass}>
+              {statusText}
+            </Badge>
+          </div>
+          <div>
+            <p className="text-sm font-semibold">Código de recogida:</p>
+            <p className="text-sm">{orderById?.pickupCode || "--"}</p>
+          </div>
+          <div>
+            <p className="text-sm font-semibold">Fecha de recogida:</p>
+            <p className="text-sm">
+              {orderById?.pickupTime
+                ? format(new Date(orderById.pickupTime), "PPPp", {
+                    locale: es,
+                  })
+                : "--"}
+            </p>
+          </div>
+          <div>
+            <p className="text-sm font-semibold">Dirección de recogida:</p>
+            <p className="text-sm">{orderById?.pickupAddress || "--"}</p>
+          </div>
+        </div>
+
+        <h4 className="mb-2 text-lg font-semibold">Productos:</h4>
+        {orderById?.cart?.products?.length > 0 ? (
+          <div className="space-y-4">
+            {orderById.cart.products.map((product) => (
+              <div
+                key={product.id}
+                className="flex items-center justify-between rounded-md bg-gray-100 p-2 shadow-sm"
+              >
+                <div>
+                  <p className="text-sm font-semibold">{product.name}</p>
+                  <p className="text-sm">Código: {product.id}</p>
+                  <p className="text-sm">Categoría: {product.category.name}</p>
+                  <p className="text-sm">
+                    Precio: S/. {product.price.toFixed(2)} x {product.quantity}{" "}
+                    unidades
+                  </p>
+                </div>
               </div>
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p className="mb-4 text-sm">No hay productos en este pedido.</p>
-      )}
-    </div>
+            ))}
+          </div>
+        ) : (
+          <p className="text-sm">No hay productos en este pedido.</p>
+        )}
+      </CardContent>
+    </Card>
   );
 }
