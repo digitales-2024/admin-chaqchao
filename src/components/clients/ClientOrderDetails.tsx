229c9ed8c912a8593caaa3e508f7175820c9a12b
FrancoGT <francogt8000@gmail.com>
2024-10-11 10:37:10 -0500

    feat: mejora en los estilos del voucher detalle

--- src/components/clients/ClientOrderDetails.tsx
+++ src/components/clients/ClientOrderDetails.tsx
@@ -1,13 +1,26 @@
 "use client";
 
 import { useOrders } from "@/hooks/use-orders";
+import { AvatarImage } from "@radix-ui/react-avatar";
 import { format } from "date-fns";
 import { es } from "date-fns/locale";
+import { Coffee } from "lucide-react";
 
-import { translateStatus } from "../orders/OrderSheetDetails";
+import { statusColors, translateStatus } from "../orders/OrderSheetDetails";
+import { Avatar, AvatarFallback } from "../ui/avatar";
 import { Badge } from "../ui/badge";
 import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
 import { ScrollArea } from "../ui/scroll-area";
+import { Separator } from "../ui/separator";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "../ui/table";
 
 interface ClientOrderDetailsProps {
   orderId: string;
@@ -26,92 +39,99 @@ export function ClientOrderDetails({ orderId }: ClientOrderDetailsProps) {
     );
   }
 
-  let badgeClass = "bg-gray-100 text-gray-500";
-  const statusText = translateStatus[orderById?.orderStatus] || "Desconocido";
-
-  switch (orderById?.orderStatus) {
-    case "PENDING":
-      badgeClass = "bg-yellow-100 text-yellow-500";
-      break;
-    case "CONFIRMED":
-      badgeClass = "bg-blue-100 text-blue-500";
-      break;
-    case "COMPLETED":
-      badgeClass = "bg-green-100 text-green-500";
-      break;
-    case "READY":
-      badgeClass = "bg-purple-100 text-purple-500";
-      break;
-    case "CANCELLED":
-      badgeClass = "bg-red-100 text-red-500";
-      break;
-    default:
-      badgeClass = "bg-gray-100 text-gray-500";
-      break;
-  }
-
   return (
-    <Card className="h-full max-h-[500px] overflow-hidden rounded-lg border shadow-lg">
+    <Card className="h-full border-none shadow-none">
       <CardHeader className="rounded-t-lg p-4">
-        <CardTitle className="text-xl font-bold">Detalles del Pedido</CardTitle>
+        <CardTitle className="text-xl font-light uppercase">
+          Pedido # {orderById?.pickupCode}
+        </CardTitle>
       </CardHeader>
-      <CardContent className="h-full p-0">
-        {/* Usamos ScrollArea de Radix para permitir el desplazamiento */}
-        <ScrollArea className="h-[400px] max-h-[400px]">
-          <div className="space-y-4 p-4">
-            <div className="grid grid-cols-2 gap-4">
-              <div>
-                <p className="text-sm font-semibold">Estado:</p>
-                <Badge variant="secondary" className={badgeClass}>
-                  {statusText}
-                </Badge>
-              </div>
-              <div>
-                <p className="text-sm font-semibold">Código de recogida:</p>
-                <p className="text-sm">{orderById?.pickupCode || "--"}</p>
-              </div>
-              <div>
-                <p className="text-sm font-semibold">Fecha de recogida:</p>
-                <p className="text-sm">
-                  {orderById?.pickupTime
-                    ? format(new Date(orderById.pickupTime), "PPPp", {
-                        locale: es,
-                      })
-                    : "--"}
-                </p>
-              </div>
-              <div>
-                <p className="text-sm font-semibold">Dirección de recogida:</p>
-                <p className="text-sm">{orderById?.pickupAddress || "--"}</p>
-              </div>
+      <CardContent>
+        <ScrollArea className="flex h-fit min-h-[400px] flex-col gap-4">
+          <div className="flex flex-col gap-2">
+            <div className="flex justify-between text-slate-500">
+              Fecha pedido
+              <span className="text-sm font-semibold text-black">
+                {format(orderById.pickupTime, "PPPp", { locale: es })}
+              </span>
             </div>
-
-            <h4 className="mb-2 text-lg font-semibold">Productos:</h4>
-            {orderById?.cart?.products?.length > 0 ? (
-              <div className="space-y-4">
-                {orderById.cart.products.map((product) => (
-                  <div
-                    key={product.id}
-                    className="flex items-center justify-between rounded-md bg-gray-100 p-3 shadow-sm"
-                  >
-                    <div>
-                      <p className="text-sm font-semibold">{product.name}</p>
-                      <p className="text-sm">Código: {product.id}</p>
-                      <p className="text-sm">
-                        Categoría: {product.category.name}
-                      </p>
-                      <p className="text-sm">
-                        Precio: S/. {product.price.toFixed(2)} x{" "}
-                        {product.quantity} unidades
-                      </p>
-                    </div>
-                  </div>
-                ))}
-              </div>
-            ) : (
-              <p className="text-sm">No hay productos en este pedido.</p>
-            )}
+            <div className="flex justify-between text-slate-500">
+              Estado
+              <Badge
+                variant="outline"
+                className={`text-sm font-semibold uppercase ${
+                  statusColors[orderById.orderStatus]
+                }`}
+              >
+                {translateStatus[orderById.orderStatus]}
+              </Badge>
+            </div>
           </div>
+          <Separator className="my-4" />
+          <div>
+            <h4 className="font-semibold">Información del Cliente</h4>
+            <div className="flex justify-between text-sm text-slate-500">
+              Nombre:
+              <span className="capitalize">{orderById.client.name}</span>
+            </div>
+            <div className="flex justify-between text-sm text-slate-500">
+              Email:
+              <span className="">{orderById.client.email}</span>
+            </div>
+            <div className="flex justify-between text-sm text-slate-500">
+              Teléfono:
+              <span className="">{orderById.client.phone}</span>
+            </div>
+          </div>
+          <Separator className="my-4" />
+          <Table>
+            <TableHeader>
+              <TableHead></TableHead>
+              <TableHead>Nombre</TableHead>
+              <TableHead>Cantidad</TableHead>
+              <TableHead>Precio/U</TableHead>
+              <TableHead>Precio Total</TableHead>
+            </TableHeader>
+            <TableBody>
+              {orderById.cart.products.map((product) => (
+                <TableRow key={product.id}>
+                  <TableCell>
+                    <Avatar className="rounded-lg">
+                      <AvatarImage src={product.image} />
+                      <AvatarFallback>
+                        <Coffee />
+                      </AvatarFallback>
+                    </Avatar>
+                  </TableCell>
+                  <TableCell>{product.name}</TableCell>
+                  <TableCell>
+                    <span className="text-xs text-slate-400">x</span>{" "}
+                    {product.quantity}
+                  </TableCell>
+                  <TableCell>
+                    <span className="text-slate-500">S/.</span>
+                    {product.price}
+                  </TableCell>
+                  <TableCell>
+                    <span className="text-slate-500">S/.</span>
+                    {product.price * product.quantity}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+            <TableFooter>
+              <TableCell colSpan={orderById.cart.products.length + 1}>
+                Total
+              </TableCell>
+              <TableCell colSpan={orderById.cart.products.length}>
+                <span className="text-slate-500">S/.</span>
+                {orderById.cart.products.reduce(
+                  (ac, va) => ac + va.quantity * va.price,
+                  0,
+                )}
+              </TableCell>
+            </TableFooter>
+          </Table>
         </ScrollArea>
       </CardContent>
     </Card>
