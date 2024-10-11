1b1980d9248491c2683ee848d5a6b454c6c55116
EleoRomario <eleo.romario@gmail.com>
2024-10-11 17:21:48 -0500

    refactor: ajustar validación del precio en el esquema de creación de productos

--- src/schemas/products/createProductsSchema.ts
+++ src/schemas/products/createProductsSchema.ts
@@ -6,12 +6,16 @@ export const productsSchema = z.object({
     .min(2, { message: "El nombre debe tener al menos 2 caracteres" }),
   description: z.string().optional(),
   price: z
-    .number({
+    .string({
       required_error: "El precio es obligatorio y debe ser mayor que 0",
     })
     .min(1, {
       message: "El precio debe ser mayor que 0",
+    })
+    .regex(/^[0-9]+(\.[0-9]{1,2})?$/, {
+      message: "El precio debe ser un número válido",
     }),
+
   isRestricted: z.boolean().optional(),
   image: z.instanceof(File).optional(),
   categoryId: z.string().min(1, { message: "Debes seleccionar una categoría" }),
