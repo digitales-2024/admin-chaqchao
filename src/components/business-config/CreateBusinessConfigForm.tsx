331b5a5cd22b8172508fa61674fe64e16bd92f48
EleoRomario <eleo.romario@gmail.com>
2024-12-07 10:41:34 -0500

    feat: agregar soporte para RUC en la configuración del negocio
    Obtener datos del cliente desde la API de SUNAT

--- src/components/business-config/CreateBusinessConfigForm.tsx
+++ src/components/business-config/CreateBusinessConfigForm.tsx
@@ -1,5 +1,7 @@
 "use client";
 import { CreateBusinessConfigSchema } from "@/schemas/businessInformation/createBusinessConfigSchema";
+import { RefreshCcw } from "lucide-react";
+import { useEffect, useState } from "react";
 import { UseFormReturn } from "react-hook-form";
 
 import { Button } from "@/components/ui/button";
@@ -12,6 +14,8 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+
+import { getCustomerData } from "@/lib/api/api-sunat";
 
 interface CreateBusinessConfigFormProps
   extends Omit<React.ComponentPropsWithRef<"form">, "onSubmit"> {
@@ -28,10 +32,53 @@ export const CreateBusinessConfigForm = ({
     formState: { isDirty },
   } = form;
 
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (!form.getValues().ruc) return;
+
+    const fetchData = async () => {
+      setIsLoading(true);
+      const response = await getCustomerData("ruc", form.getValues().ruc);
+
+      setIsLoading(false);
+      form.setValue("businessName", response.razonSocial);
+      form.setValue("address", response.direccion);
+    };
+
+    if (form.getValues().ruc.length === 11) {
+      fetchData();
+    } else {
+      form.setValue("businessName", "");
+      form.setValue("address", "");
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [form.watch("ruc")]);
+
   return (
     <Form {...form}>
       <form onSubmit={handleSubmit(onSubmit)} className="space-y-8">
         <div className="flex flex-col gap-6 p-4 sm:p-0">
+          <FormField
+            control={form.control}
+            name="ruc"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel htmlFor="ruc">RUC del Negocio</FormLabel>
+                <FormControl>
+                  <>
+                    <Input id="ruc" placeholder="ruc" {...field} />
+                    {isLoading && (
+                      <span>
+                        <RefreshCcw className="absolute right-2 top-2 animate-spin text-primary" />
+                      </span>
+                    )}
+                  </>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
           <FormField
             control={form.control}
             name="businessName"
