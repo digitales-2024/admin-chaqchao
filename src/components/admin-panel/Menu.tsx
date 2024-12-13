562cfd77ab111e34139cfe7790268f7d4b8d4ff0
EleoRomario <eleo.romario@gmail.com>
2024-12-13 10:29:34 -0500

    feat: mejorar lógica de menú para mostrar elementos según la visibilidad

--- src/components/admin-panel/Menu.tsx
+++ src/components/admin-panel/Menu.tsx
@@ -4,7 +4,7 @@ import { Ellipsis, LogOut } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-import { getMenuList } from "@/lib/menu-list";
+import { GetMenuList } from "@/lib/menu-list";
 import { cn } from "@/lib/utils";
 
 import { Button } from "../ui/button";
@@ -23,8 +23,7 @@ interface MenuProps {
 
 export const Menu = ({ isOpen }: MenuProps) => {
   const pathname = usePathname() ?? ""; // Si pathname es null, se usará ""
-  const menuList = getMenuList(pathname);
-
+  const menuList = GetMenuList(pathname);
   const { signOut } = useLogout();
 
   return (
@@ -54,68 +53,72 @@ export const Menu = ({ isOpen }: MenuProps) => {
                 <p className="pb-2"></p>
               )}
               {menus.map(
-                ({ href, label, icon: Icon, active, submenus }, index) =>
-                  submenus.length === 0 ? (
-                    <div className="w-full" key={index}>
-                      <TooltipProvider disableHoverableContent>
-                        <Tooltip delayDuration={100}>
-                          <TooltipTrigger asChild>
-                            <Button
-                              variant={active ? "secondary" : "ghost"}
-                              className={cn(
-                                "group/btn-link mb-1 h-10 w-full justify-start border-l-4 border-transparent text-slate-600 transition-colors duration-300 hover:border-primary",
-                                {
-                                  "border-primary text-primary": active,
-                                },
-                              )}
-                              asChild
-                            >
-                              <Link href={href}>
-                                <span
-                                  className={cn(isOpen === false ? "" : "mr-4")}
-                                >
-                                  <Icon
-                                    size={18}
-                                    className={cn(
-                                      "transition-colors duration-200 group-hover/btn-link:text-primary",
-                                      {
-                                        "text-primary": active,
-                                      },
-                                    )}
-                                  />
-                                </span>
-                                <p
+                ({ href, label, icon: Icon, active, submenus, show }, index) =>
+                  submenus.length === 0
+                    ? show && (
+                        <div className="w-full" key={index}>
+                          <TooltipProvider disableHoverableContent>
+                            <Tooltip delayDuration={100}>
+                              <TooltipTrigger asChild>
+                                <Button
+                                  variant={active ? "secondary" : "ghost"}
                                   className={cn(
-                                    "max-w-[200px] truncate",
-                                    isOpen === false
-                                      ? "-translate-x-96 opacity-0"
-                                      : "translate-x-0 opacity-100",
+                                    "group/btn-link mb-1 h-10 w-full justify-start border-l-4 border-transparent text-slate-600 transition-colors duration-300 hover:border-primary",
+                                    {
+                                      "border-primary text-primary": active,
+                                    },
                                   )}
+                                  asChild
                                 >
+                                  <Link href={href}>
+                                    <span
+                                      className={cn(
+                                        isOpen === false ? "" : "mr-4",
+                                      )}
+                                    >
+                                      <Icon
+                                        size={18}
+                                        className={cn(
+                                          "transition-colors duration-200 group-hover/btn-link:text-primary",
+                                          {
+                                            "text-primary": active,
+                                          },
+                                        )}
+                                      />
+                                    </span>
+                                    <p
+                                      className={cn(
+                                        "max-w-[200px] truncate",
+                                        isOpen === false
+                                          ? "-translate-x-96 opacity-0"
+                                          : "translate-x-0 opacity-100",
+                                      )}
+                                    >
+                                      {label}
+                                    </p>
+                                  </Link>
+                                </Button>
+                              </TooltipTrigger>
+                              {isOpen === false && (
+                                <TooltipContent side="right">
                                   {label}
-                                </p>
-                              </Link>
-                            </Button>
-                          </TooltipTrigger>
-                          {isOpen === false && (
-                            <TooltipContent side="right">
-                              {label}
-                            </TooltipContent>
-                          )}
-                        </Tooltip>
-                      </TooltipProvider>
-                    </div>
-                  ) : (
-                    <div className="w-full" key={index}>
-                      <CollapseMenuButton
-                        icon={Icon}
-                        label={label}
-                        active={active}
-                        submenus={submenus}
-                        isOpen={isOpen}
-                      />
-                    </div>
-                  ),
+                                </TooltipContent>
+                              )}
+                            </Tooltip>
+                          </TooltipProvider>
+                        </div>
+                      )
+                    : show && (
+                        <div className="w-full" key={index}>
+                          <CollapseMenuButton
+                            icon={Icon}
+                            label={label}
+                            active={active}
+                            submenus={submenus}
+                            isOpen={isOpen}
+                          />
+                        </div>
+                      ),
               )}
             </li>
           ))}
