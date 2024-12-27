2b01c4d86346e4e7b15a089a695895c725264803
EleoRomario <eleo.romario@gmail.com>
2024-12-27 12:44:07 -0500

    feat: agregar componente de resumen de clase con detalles de fecha, hora y participantes

---
+++ src/components/class/SummaryClass.tsx
@@ -0,0 +1,55 @@
+"use client";
+import { createClassSchema } from "@/schemas";
+import { TypeClass, typeClassLabels } from "@/types";
+import { format } from "date-fns";
+import { es } from "date-fns/locale";
+import { CalendarDays, Clock, UsersRound } from "lucide-react";
+import { UseFormReturn } from "react-hook-form";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+interface SummaryClassProps {
+  class: UseFormReturn<createClassSchema>;
+}
+export const SummaryClass = ({ class: classForm }: SummaryClassProps) => {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Resumen de la clase</CardTitle>
+        <CardDescription>
+          Clase tipo{" "}
+          {typeClassLabels[classForm.getValues().typeClass as TypeClass]}
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <ul className="flex flex-col gap-2">
+          <li className="inline-flex items-center gap-3 text-sm text-gray-600">
+            <CalendarDays className="size-4 text-cyan-500" /> Día:
+            <span className="font-semibold text-gray-900">
+              {format(classForm.getValues().dateClass, "PPP", { locale: es })}
+            </span>
+          </li>
+          <li className="inline-flex items-center gap-3 text-sm text-gray-600">
+            <Clock className="size-4 text-cyan-500" /> Hora:
+            <span className="font-semibold text-gray-900">
+              {classForm.getValues().scheduleClass}
+            </span>
+          </li>
+          <li className="inline-flex items-center gap-3 text-sm text-gray-600">
+            <UsersRound className="size-4 text-cyan-500" /> Participantes:{" "}
+            <span className="font-semibold text-gray-900">
+              {classForm.getValues().totalAdults +
+                classForm.getValues().totalChildren}
+            </span>
+          </li>
+        </ul>
+      </CardContent>
+    </Card>
+  );
+};
